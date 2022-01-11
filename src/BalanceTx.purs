71287561cdca9265ea639e2a0a9b7ed25e0f6dc7
vvtran <viet.tran.vvt@googlemail.com>
2022-01-11 10:23:50 +0000

    eq instances to TokenName, CurrencySymbol & Value, starting balancing tx

--- src/BalanceTx.purs
+++ src/BalanceTx.purs
@@ -1,2 +1,36 @@
 module BalanceTx where
 
+import Prelude
+import Data.Either (Either(..))
+import Map (Map)
+import Map as Map
+
+import Types.JsonWsp (UtxoQueryResult)
+
+-- This module replicates functionality from
+-- https://github.com/mlabs-haskell/mlabs-pab/blob/master/src/MLabsPAB/PreBalance.hs
+
+addTxCollaterals
+  :: UtxoQueryResult
+  -> Transaction
+  -> Either String Transaction
+addTxCollaterals utxos tx = do
+  pure tx
+  where
+    filterAdaOnly :: 
+
+{- | Pick a collateral from the utxo map and add it to the unbalanced transaction
+ (suboptimally we just pick a random utxo from the tx inputs)
+-}
+-- addTxCollaterals :: Map TxOutRef TxOut -> Tx -> Either Text Tx
+-- addTxCollaterals utxos tx = do
+--   let txIns = mapMaybe (rightToMaybe . txOutToTxIn) $ Map.toList $ filterAdaOnly utxos
+--   txIn <- findPubKeyTxIn txIns
+--   pure $ tx {txCollateral = Set.singleton txIn}
+--   where
+--     findPubKeyTxIn = \case
+--       x@(TxIn _ (Just ConsumePublicKeyAddress)) : _ -> Right x
+--       x@(TxIn _ Nothing) : _ -> Right x
+--       _ : xs -> findPubKeyTxIn xs
+--       _ -> Left "There are no utxos to be used as collateral"
+--     filterAdaOnly = Map.filter (isAdaOnly . txOutValue)
