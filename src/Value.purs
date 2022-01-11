2bae89ddda7172d550a3362623b5d7523f92434d
vvtran <viet.tran.vvt@googlemail.com>
2022-01-11 11:48:11 +0000

    more explicit imports with .. however

--- src/Value.purs
+++ src/Value.purs
@@ -3,32 +3,33 @@ module Value
   ) where
 
 import Prelude
+
 import Control.Alternative (guard)
-import Data.BigInt as BigInt
+import Data.BigInt (BigInt)
 import Data.List ((:), List(..))
 import Data.Map (Map, toUnfoldable)
 import Data.Tuple.Nested ((/\), type (/\))
 
-import Types.Transaction as Transaction
+import Types.Transaction (CurrencySymbol(..), TokenName(..), Value(..))
 
 -- Could use Data.Newtype (unwrap) too.
 getValue
-  :: Transaction.Value
-  -> Map Transaction.CurrencySymbol (Map Transaction.TokenName BigInt.BigInt)
-getValue (Transaction.Value v) = v
+  :: Value
+  -> Map CurrencySymbol (Map TokenName BigInt)
+getValue (Value v) = v
 
 -- Taken from https://staging.plutus.iohkdev.io/doc/haddock/plutus-ledger-api/html/src/Plutus.V1.Ledger.Value.html#flattenValue
 flattenValue
-  :: Transaction.Value
-  -> List (Transaction.CurrencySymbol /\ Transaction.TokenName /\ BigInt.BigInt)
+  :: Value
+  -> List (CurrencySymbol /\ TokenName /\ BigInt)
 flattenValue v = do
     cs /\ m <- toUnfoldable <<< getValue $ v
     tn /\ a <- toUnfoldable m
     guard $ a /= zero
     pure $ cs /\ tn /\ a
 
-isAdaOnly :: Transaction.Value -> Boolean
+isAdaOnly :: Value -> Boolean
 isAdaOnly v =
   case flattenValue v of
-    (Transaction.CurrencySymbol "" /\ Transaction.TokenName "" /\ _) : Nil -> true
+    (CurrencySymbol "" /\ TokenName "" /\ _) : Nil -> true
     _ -> false
