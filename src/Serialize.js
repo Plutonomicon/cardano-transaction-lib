5783b4dc19b07ff640aefe9cdd33772935e6f4db
Vladimir Kalnitsky <vladimir@mlabs.city>
2022-01-19 19:36:00 +0300

    Use arrow functions in JS code

--- src/Serialize.js
+++ src/Serialize.js
@@ -1,101 +1,44 @@
 var lib = // require('@emurgo/cardano-serialization-lib-nodejs');
     require('../node_modules/cardano-serialization-lib/publish/cardano_serialization_lib.js');
 
-exports.newTransactionBody = function (inputs) {
-    return function (outputs) {
-        return function (fee) {
-            return function () {
-                lib.TransactionBody.new(inputs, outputs, fee /* ttl */);
-            };
-        };
-    };
-};
+exports.newTransactionBody = inputs => outputs => fee => () =>
+    lib.TransactionBody.new(inputs, outputs, fee /* ttl */);
 
-exports.newTransaction = function (body) {
-    return function (witness_set) {
-        return function () {
-            return lib.Transaction.new(body, witness_set /* auxiliary_data */);
-        };
-    };
-};
+exports.newTransaction = body => witness_set => () =>
+    lib.Transaction.new(body, witness_set /* auxiliary_data */);
 
-exports.newTransaction_ = function (body) {
-    return function (witness_set) {
-        return function (auxiliary_data) {
-            return function () {
-                return lib.Transaction.new(body, witness_set, auxiliary_data);
-            };
-        };
-    };
-};
+exports.newTransaction_ = body => witness_set => auxiliary_data => () =>
+    lib.Transaction.new(body, witness_set, auxiliary_data);
 
-exports.newTransactionInputs = function () {
-    return lib.TransactionInputs.new();
-};
+exports.newTransactionInputs = () =>
+    lib.TransactionInputs.new();
 
-exports.addTransactionInput = function (inputs) {
-    return function (input) {
-        return function () {
-            inputs.add(input);
-        };
-    };
-};
+exports.addTransactionInput = inputs => input => () =>
+    inputs.add(input);
 
-exports.newTransactionOutputs = function () {
-    return lib.TransactionOutputs.new();
-};
+exports.newTransactionOutputs = () =>
+    lib.TransactionOutputs.new();
 
-exports.addTransactionOutput = function (outputs) {
-    return function (output) {
-        return function () {
-            outputs.add(output);
-        };
-    };
-};
+exports.addTransactionOutput = outputs => output => () =>
+    outputs.add(output);
 
-exports.newTransactionHash = function (hash) {
-    return function () {
-        return lib.TransactionHash.from_bech32(hash);
-    };
-};
+exports.newTransactionHash = hash => () =>
+    lib.TransactionHash.from_bech32(hash);
 
-exports.newTransactionInput = function (transaction_id) {
-    return function (index) {
-        return function () {
-            return lib.TransactionInput.new(transaction_id, index);
-        };
-    };
-};
+exports.newTransactionInput = transaction_id => index => () =>
+    lib.TransactionInput.new(transaction_id, index);
 
-exports.newTransactionOutput = function (address) {
-    return function (amount) {
-        return function () {
-            return lib.TransactionOutput.new(address, amount);
-        };
-    };
-};
+exports.newTransactionOutput = address => amount => () =>
+    lib.TransactionOutput.new(address, amount);
 
-exports.newTransactionWitnessSet = function () {
-    return lib.TransactionWitnessSet.new();
-};
+exports.newTransactionWitnessSet = () =>
+    lib.TransactionWitnessSet.new();
 
-exports.newAddress = function (bech_str) {
-    return function () {
-        return lib.Address.from_bech32(bech_str);
-    };
-};
+exports.newBigNum = string => () =>
+    lib.BigNum.from_str(string);
 
-exports.newBigNum = function (string) {
-    return function () {
-        return lib.BigNum.new(string);
-    };
-};
-
-exports.newValue = function (coin) {
-    return function () {
-        return lib.Value.new(coin);
-    };
-};
+exports.newValue = coin => () =>
+    lib.Value.new(coin);
 
 exports.newBaseAddress = network => payment => stake => () =>
     lib.BaseAddress.new(network, payment, stake);
