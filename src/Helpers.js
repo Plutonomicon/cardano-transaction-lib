60a20ecd4728db46ffe58efea0b1b7d0330fe855
anks <michal.anks@gmail.com>
2022-01-23 17:59:38 +0100

    Fix the string escaping bug.

--- src/Helpers.js
+++ src/Helpers.js
@@ -7,14 +7,21 @@ exports.jsonTurnNumbersToStrings = (str) => {
     var prev_in_number = false;
     var in_number = false;
     var in_string = false;
+    var escaped = -1;
 
     for (var i = 0, n = s.length; i < n; ++i) {
         const c = s[i];
-        if (c == '"') {
-            if( i ==0 ||  (i>0 && s[i-1] != "\\" )){
-                in_string = !in_string;
+        // set the escape flag
+        if (in_string && escaped!=i){
+            if (c == "\\"){
+                escaped = i+1;
             }
         }
+        // set in_string flag
+        if (c == '"' && escaped!=i) {
+            in_string = !in_string;
+        }
+        // set in_number flag and quote numbers
         if(!in_string){
             if (c >= '0' && c <= '9' || c == '-') {
                 in_number=true;
@@ -33,10 +40,12 @@ exports.jsonTurnNumbersToStrings = (str) => {
             }
             prev_in_number = in_number;
         }
+        // push char if in string or is not a whitespace
         if(in_string || !/\s/.test(c)){
             arr.push(c);
         }
     }
+    // simple number case - close with quote
     if (!in_string && in_number){
         arr.push("\"");
     }
