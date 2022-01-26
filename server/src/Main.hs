37edca7653bec469db74c2a2060620860a888013
Rory Tyler Hayford <rory.hayford@protonmail.com>
2022-01-26 16:37:26 +0700

    Read protocol params at startup
    Logger settings config

--- server/src/Main.hs
+++ server/src/Main.hs
@@ -1,11 +1,26 @@
 module Main (main) where
 
-import Api
-import Network.Wai.Handler.Warp (runSettings)
+import Api (app)
+import Cardano.Api.Shelley qualified as C
+import Data.Aeson qualified as Aeson
+import Data.Function ((&))
+import Network.Wai.Handler.Warp (
+  defaultSettings,
+  runSettings,
+  setLogger,
+  setPort,
+ )
 import Network.Wai.Logger (withStdoutLogger)
+import Paths_cardano_browser_tx_server (getDataFileName)
+import System.Exit (die)
+import Types (Env (..))
 
 main :: IO ()
-main = withStdoutLogger $ \logger ->
-  runSettings (mkSettings logger) . app $ undefined -- TODO new env
+main = withStdoutLogger $ \logger -> do
+  protocolParams <-
+    either die pure
+      =<< Aeson.eitherDecodeFileStrict @C.ProtocolParameters
+      =<< getDataFileName "config/pparams.json"
+  runSettings (mkSettings logger) . app $ Env {..}
   where
-    mkSettings = undefined
+    mkSettings logger = defaultSettings & setPort 8081 & setLogger logger
