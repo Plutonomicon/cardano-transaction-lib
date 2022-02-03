4d37c3b549ac10631d3180e83c3f564afc6756c1
Vladimir Kalnitsky <vladimir@mlabs.city>
2022-02-03 18:44:52 +0300

    Refactor Deserialization test

--- test/Deserialization.purs
+++ test/Deserialization.purs
@@ -17,13 +17,9 @@ suite = do
   group "cardano-serialization-lib bindings (deserialization)" $ do
     group "Address" $ do
       test "deserialization works" do
-        let
-          addressString = "addr1qyc0kwu98x23ufhsxjgs5k3h7gktn8v5682qna5amwh2juguztcrc8hjay66es67ctn0jmr9plfmlw37je2s2px4xdssgvxerq"
         address <- liftEffect $ Serialization.newAddressFromBech32 (T.Bech32 addressString)
         Deserialization.convertAddress address `shouldSatisfy` isJust
       test "deserialization is inverse to serialization" do
-        let
-          addressString = "addr1qyc0kwu98x23ufhsxjgs5k3h7gktn8v5682qna5amwh2juguztcrc8hjay66es67ctn0jmr9plfmlw37je2s2px4xdssgvxerq"
         address <- liftEffect $ Serialization.newAddressFromBech32 (T.Bech32 addressString)
         liftEffect case Deserialization.convertAddress address of
           Nothing -> throw "Failed deserialization"
@@ -33,3 +29,6 @@ suite = do
               Nothing -> throw "Failed deserialization"
               Just address''' -> do
                 address''' `shouldEqual` address'
+
+addressString :: String
+addressString = "addr1qyc0kwu98x23ufhsxjgs5k3h7gktn8v5682qna5amwh2juguztcrc8hjay66es67ctn0jmr9plfmlw37je2s2px4xdssgvxerq"
