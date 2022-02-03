1d8dbf293c6d82d57b2b7a86d90d10d0e5f62d2a
Rory Tyler Hayford <rory.hayford@protonmail.com>
2022-02-03 14:09:02 +0700

    Add webpack config

---
+++ webpack.config.js
@@ -0,0 +1,92 @@
+"use strict";
+
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const webpack = require("webpack");
+const isWebpackDevServer = process.argv.some(
+  (a) => path.basename(a) === "webpack-dev-server"
+);
+const isWatch = process.argv.some((a) => a === "--watch");
+
+const plugins =
+  isWebpackDevServer || !isWatch
+    ? []
+    : [
+        function () {
+          this.plugin("done", function (stats) {
+            process.stderr.write(stats.toString("errors-only"));
+          });
+        },
+      ];
+module.exports = {
+  experiments: {
+    asyncWebAssembly: false,
+    layers: false,
+    lazyCompilation: false,
+    outputModule: true,
+    syncWebAssembly: true,
+    topLevelAwait: true,
+  },
+
+  devtool: "eval-source-map",
+
+  stats: { errorDetails: true },
+
+  devServer: {
+    port: 4008,
+  },
+
+  // we can add more entrypoints as needed
+  entry: "./src/index.js",
+
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.js",
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.purs$/,
+        use: [
+          {
+            loader: "purs-loader",
+            options: {
+              src: ["src/**/*.purs", "test/**/*.purs", "examples/**/*.purs"],
+              spago: true,
+              watch: isWebpackDevServer || isWatch,
+              pscIde: true,
+            },
+          },
+        ],
+      },
+      {
+        test: /\.(png|jpg|gif)$/i,
+        use: [
+          {
+            loader: "url-loader",
+            options: {
+              limit: 8192,
+            },
+          },
+        ],
+      },
+    ],
+  },
+
+  resolve: {
+    modules: ["node_modules"],
+    extensions: [".purs", ".js"],
+  },
+
+  plugins: [
+    new webpack.LoaderOptionsPlugin({
+      debug: true,
+    }),
+    new HtmlWebpackPlugin({
+      title: "purescript-webpack-example",
+      template: "index.html",
+      inject: false, // See stackoverflow.com/a/38292765/3067181
+    }),
+  ].concat(plugins),
+};
