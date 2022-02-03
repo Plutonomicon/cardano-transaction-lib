e30ea370897cbf454829aa969f240975fd0fae4e
Rory Tyler Hayford <rory.hayford@protonmail.com>
2022-02-03 20:13:04 +0700

    Add simple usage example

--- examples/nami/Simple.purs
+++ examples/nami/Simple.purs
@@ -1,9 +1,21 @@
-module Examples.Nami.Simple where
+module Examples.Nami.Simple (main) where
 
 import Prelude
 
 import Effect (Effect)
+import Effect.Aff (Aff, launchAff_)
+import Effect.Class (liftEffect)
 import Effect.Console as Console
+import Effect.Ref as Ref
+import Wallet (Wallet(..), mkNamiWalletAff)
+
 
 main :: Effect Unit
-main = Console.log "Hello browser"
+main = launchAff_ $ logWalletAddress
+
+logWalletAddress :: Aff Unit
+logWalletAddress = do
+  Nami nami <- mkNamiWalletAff
+  conn <- liftEffect $ Ref.read nami.connection
+  address <- nami.getWalletAddress conn
+  liftEffect $ Console.log $ show address
