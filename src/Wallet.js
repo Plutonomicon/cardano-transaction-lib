51eec75169ad4274fc7c13d87e9a87dc738fce22
Rory Tyler Hayford <rory.hayford@protonmail.com>
2022-02-07 15:09:32 +0700

    Add FFI binding

--- src/Wallet.js
+++ src/Wallet.js
@@ -4,3 +4,9 @@ exports._enableNami = () => window.cardano.nami.enable();
 // _getNamiAddress :: NamiConnection -> Effect (Promise String)
 exports._getNamiAddress = (nami) => () =>
   nami.getUsedAddresses().then((addrs) => addrs[0]);
+
+// _getNamiCollateral
+//   :: NamiConnection
+//   -> Effect (Promise String)
+exports._getNamiCollateral = (nami) => () =>
+  nami.experimental.getCollateral().then((utxos) => utxos[0]);
