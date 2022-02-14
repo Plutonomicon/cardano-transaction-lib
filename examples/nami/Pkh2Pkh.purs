deddd25cf138e6b9131644fd6e71bc78b9bae5ef
Rory Tyler Hayford <rory.hayford@protonmail.com>
2022-02-14 11:31:35 +0700

    WIP: build tx and tx body

--- examples/nami/Pkh2Pkh.purs
+++ examples/nami/Pkh2Pkh.purs
@@ -2,14 +2,33 @@ module Examples.Nami.Pkh2Pkh (main) where
 
 import Prelude
 
-import BalanceTx (UnbalancedTransaction)
 import Control.Monad.Error.Class (throwError)
 import Control.Monad.Reader (runReaderT)
+import Data.Array as Array
+import Data.BigInt as BigInt
+import Data.Map as Map
 import Data.Maybe (Maybe(..), maybe)
+import Data.Newtype (unwrap)
+import Data.Tuple (fst)
 import Effect (Effect)
 import Effect.Aff (error, launchAff_)
 import Effect.Aff.Class (liftAff)
-import QueryM (QueryM, defaultServerConfig, getWalletAddress)
+import QueryM (QueryM, defaultServerConfig, getWalletAddress, utxosAt)
+import Types.POSIXTimeRange
+  ( Extended(..)
+  , Interval(..)
+  , LowerBound(..)
+  , UpperBound(..)
+  )
+import Types.Transaction
+  ( NetworkId(..)
+  , Transaction(..)
+  , TransactionOutput(..)
+  , TxBody(..)
+  , emptyTransactionWitnessSet
+  )
+import Types.UnbalancedTransaction (UnbalancedTx(..))
+import Types.Value as Value
 import Undefined (undefined)
 import Wallet (mkNamiWalletAff)
 
@@ -24,14 +43,55 @@ main = launchAff_ $ do
     }
   undefined
 
-buildTransaction :: QueryM UnbalancedTx
-buildTransaction = do
-  ownAddress <-
-    maybe
-      (throw "Failed to get wallet address")
-      pure
-      =<< getWalletAddress
-  undefined
+buildTransaction :: QueryM Transaction
+buildTransaction = undefined
+
+buildUnbalancedTransaction :: QueryM UnbalancedTx
+buildUnbalancedTransaction = do
+  ownAddress <- mthrow "Failed to get wallet address" getWalletAddress
+  inputs <-
+    map fst
+      <<< Map.toUnfoldable
+      <<< unwrap
+      <$> mthrow "Failed to get own utxos" (utxosAt ownAddress)
+  pure $ UnbalancedTx
+    { transaction: Transaction
+        { body: TxBody
+            { inputs
+            , outputs: Array.singleton $ TransactionOutput
+                { address: ownAddress
+                , amount: Value.lovelaceValueOf $ BigInt.fromInt 2000000
+                , data_hash: Nothing
+                }
+            -- FIXME
+            , fee: undefined
+            , network_id: Just Testnet
+            , certs: Nothing
+            , collateral: Nothing
+            , auxiliary_data_hash: Nothing
+            , mint: Nothing
+            , required_signers: Nothing
+            , script_data_hash: Nothing
+            , ttl: Nothing
+            , update: Nothing
+            , validity_start_interval: Nothing
+            , withdrawals: Nothing
+            }
+        , is_valid: true
+        , witness_set: emptyTransactionWitnessSet
+        , auxiliary_data: Nothing
+        }
+    , requiredSignatories: Map.empty
+    , utxoIndex: Map.empty
+    , validityTimeRange: Interval
+        { from: LowerBound NegInf true
+        , to: UpperBound PosInf true
+        }
+    }
 
 throw :: forall (a :: Type). String -> QueryM a
 throw = liftAff <<< throwError <<< error
+
+mthrow :: forall (a :: Type). String -> QueryM (Maybe a) -> QueryM a
+mthrow msg act = maybe (throw msg) pure =<< act
+
