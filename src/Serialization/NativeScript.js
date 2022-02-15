06eccbf7570a26a5a16c57bc426d23f5e94e9e9b
Vladimir Kalnitsky <vladimir@mlabs.city>
2022-02-15 17:52:28 +0300

    Implement serialization for NativeScript

---
+++ src/Serialization/NativeScript.js
@@ -0,0 +1,24 @@
+/* global require exports */
+
+var lib;
+if (typeof BROWSER_RUNTIME != 'undefined' && BROWSER_RUNTIME) {
+    lib = require('@ngua/cardano-serialization-lib-browser');
+} else {
+    lib = require('@ngua/cardano-serialization-lib-nodejs');
+}
+
+const mkScript = prop => arg => lib.NativeScript[prop](arg);
+
+exports.mkScriptPubkey = keyHash => lib.ScriptPubkey.new(keyHash);
+exports.nativeScript_new_script_pubkey = mkScript('new_script_pubkey');
+exports.nativeScript_new_script_all = mkScript('new_script_all');
+exports.nativeScript_new_script_any = mkScript('new_script_any');
+exports.nativeScript_new_script_n_of_k = mkScript('new_script_n_of_k');
+exports.nativeScript_new_timelock_start = mkScript('new_timelock_start');
+exports.nativeScript_new_timelock_expiry = mkScript('new_timelock_expiry');
+exports.packNativeScripts = helper => nss => helper.pack(lib.NativeScripts, nss);
+exports.mkScriptAll = nss => lib.ScriptAll.new(nss);
+exports.mkScriptAny = nss => lib.ScriptAny.new(nss);
+exports.mkScriptNOfK = n => nss => lib.ScriptNOfK.new(n, nss);
+exports.mkTimelockExpiry = n => lib.TimelockExpiry.new(n);
+exports.mkTimelockStart = n => lib.TimelockStart.new(n);
---
+++ src/Serialization/NativeScript.purs
@@ -0,0 +1,66 @@
+module Serialization.NativeScript where
+
+import Prelude
+
+import Data.BigInt as BigInt
+import Data.Maybe (Maybe)
+import Data.Traversable (for)
+
+import Deserialization.FromBytes (fromBytes)
+import FfiHelpers (ContainerHelper, containerHelper)
+import Serialization.Types (Ed25519KeyHash, NativeScript, NativeScripts, ScriptAll, ScriptAny, ScriptNOfK, ScriptPubkey, TimelockExpiry, TimelockStart)
+import Types.Transaction
+  ( NativeScript(ScriptPubkey, ScriptAll, ScriptAny, ScriptNOfK, TimelockStart, TimelockExpiry)
+  , Ed25519KeyHash(Ed25519KeyHash)
+  , Slot(Slot)
+  ) as T
+
+convertNativeScript :: T.NativeScript -> Maybe NativeScript
+convertNativeScript = case _ of
+  T.ScriptPubkey keyHash -> convertScriptPubkey keyHash
+  T.ScriptAll nss -> convertScriptAll nss
+  T.ScriptAny nss -> convertScriptAny nss
+  T.ScriptNOfK n nss -> convertScriptNOfK n nss
+  T.TimelockStart slot -> convertTimelockStart slot
+  T.TimelockExpiry slot -> convertTimelockExpiry slot
+
+convertScriptPubkey :: T.Ed25519KeyHash -> Maybe NativeScript
+convertScriptPubkey (T.Ed25519KeyHash bytes) = do
+  nativeScript_new_script_pubkey <<< mkScriptPubkey <$> fromBytes bytes
+
+convertScriptAll :: Array T.NativeScript -> Maybe NativeScript
+convertScriptAll nss =
+  nativeScript_new_script_all <<< mkScriptAll <<<
+    packNativeScripts containerHelper <$> for nss convertNativeScript
+
+convertScriptAny :: Array T.NativeScript -> Maybe NativeScript
+convertScriptAny nss =
+  nativeScript_new_script_any <<< mkScriptAny <<<
+    packNativeScripts containerHelper <$> for nss convertNativeScript
+
+convertScriptNOfK :: Int -> Array T.NativeScript -> Maybe NativeScript
+convertScriptNOfK n nss =
+  nativeScript_new_script_n_of_k <<< mkScriptNOfK n <<<
+    packNativeScripts containerHelper <$> for nss convertNativeScript
+
+convertTimelockStart :: T.Slot -> Maybe NativeScript
+convertTimelockStart (T.Slot slot) =
+  nativeScript_new_timelock_start <<< mkTimelockStart <$> BigInt.toInt slot
+
+convertTimelockExpiry :: T.Slot -> Maybe NativeScript
+convertTimelockExpiry (T.Slot slot) =
+  nativeScript_new_timelock_expiry <<< mkTimelockExpiry <$> BigInt.toInt slot
+
+foreign import mkScriptPubkey :: Ed25519KeyHash -> ScriptPubkey
+foreign import packNativeScripts :: ContainerHelper -> Array NativeScript -> NativeScripts
+foreign import mkScriptAll :: NativeScripts -> ScriptAll
+foreign import mkScriptAny :: NativeScripts -> ScriptAny
+foreign import mkScriptNOfK :: Int -> NativeScripts -> ScriptNOfK
+foreign import mkTimelockStart :: Int -> TimelockStart
+foreign import mkTimelockExpiry :: Int -> TimelockExpiry
+foreign import nativeScript_new_script_pubkey :: ScriptPubkey -> NativeScript
+foreign import nativeScript_new_script_all :: ScriptAll -> NativeScript
+foreign import nativeScript_new_script_any :: ScriptAny -> NativeScript
+foreign import nativeScript_new_script_n_of_k :: ScriptNOfK -> NativeScript
+foreign import nativeScript_new_timelock_start :: TimelockStart -> NativeScript
+foreign import nativeScript_new_timelock_expiry :: TimelockExpiry -> NativeScript
