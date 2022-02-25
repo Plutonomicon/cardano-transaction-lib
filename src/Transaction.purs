61ed863b4b23dbbffbf89769c3088e2dc66f3d69
Rory Tyler Hayford <rory.hayford@protonmail.com>
2022-02-25 13:17:40 +0700

    Return `Either` instead of `Maybe`

--- src/Transaction.purs
+++ src/Transaction.purs
@@ -1,33 +1,21 @@
-module Transaction (attachDatum) where
+module Transaction
+  ( ModifyTxError(..)
+  , attachDatum
+  ) where
 
 import Prelude
 
-import Control.Monad.Maybe.Trans (MaybeT(MaybeT), runMaybeT)
-import Data.Maybe (Maybe(Just))
+import Control.Monad.Except.Trans (ExceptT(ExceptT), runExceptT)
+import Data.Either (Either(Right), note)
 import Data.Generic.Rep (class Generic)
 import Data.Show.Generic (genericShow)
 import Effect (Effect)
+import Helpers (liftEither)
 import Serialization.WitnessSet as Serialization.WitnessSet
 import Deserialization.WitnessSet as Deserialization.WitnessSet
 import Serialization.PlutusData as Serialization.PlutusData
 import Types.PlutusData (Datum(Datum))
 import Types.Transaction (Transaction(Transaction), TransactionWitnessSet)
-
-attachDatum :: Datum -> Transaction -> Effect (Maybe Transaction)
-attachDatum (Datum pd) (Transaction tx@{ witness_set: ws }) = runMaybeT $ do
-  pd' <- liftMaybe $ Serialization.PlutusData.convertPlutusData pd
-  newWits <- MaybeT $
-    map Deserialization.WitnessSet.convertWitnessSet
-      <<< Serialization.WitnessSet.setPlutusData pd'
-      =<< Serialization.WitnessSet.convertWitnessSet ws
-  liftMaybe $ Just $ Transaction $ tx { witness_set = newWits }
-  where
-  liftMaybe
-    :: forall (a :: Type) (m :: Type -> Type)
-     . Applicative m
-    => Maybe a
-    -> MaybeT m a
-  liftMaybe = MaybeT <<< pure
 
 data ModifyTxError
   = ConvertWitsError
@@ -37,3 +25,17 @@ derive instance Generic ModifyTxError _
 
 instance Show ModifyTxError where
   show a = genericShow a
+
+attachDatum :: Datum -> Transaction -> Effect (Either ModifyTxError Transaction)
+attachDatum (Datum pd) (Transaction tx@{ witness_set: ws }) = runExceptT $ do
+  pd' <- liftEither
+    $ note ConvertDatumError
+    $ Serialization.PlutusData.convertPlutusData pd
+  newWits <- ExceptT $
+    map
+      ( note ConvertDatumError
+          <<< Deserialization.WitnessSet.convertWitnessSet
+      )
+      <<< Serialization.WitnessSet.setPlutusData pd'
+      =<< Serialization.WitnessSet.convertWitnessSet ws
+  liftEither $ Right $ Transaction $ tx { witness_set = newWits }
