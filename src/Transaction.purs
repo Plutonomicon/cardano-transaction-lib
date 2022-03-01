c127c2c29cba4bc733c299ef7dc7eefa465d07e7
Rory Tyler Hayford <rory.hayford@protonmail.com>
2022-03-01 15:03:16 +0700

    Helper for converting witnesses with setter effect

--- src/Transaction.purs
+++ src/Transaction.purs
@@ -5,7 +5,7 @@ module Transaction
 
 import Prelude
 
-import Control.Monad.Except.Trans (runExceptT)
+import Control.Monad.Except.Trans (ExceptT, runExceptT)
 import Data.Either (Either(Right), note)
 import Data.Generic.Rep (class Generic)
 import Data.Show.Generic (genericShow)
@@ -14,13 +14,19 @@ import Effect (Effect)
 import Effect.Class (liftEffect)
 import Helpers (liftEither)
 import Serialization.PlutusData as Serialization.PlutusData
+import Serialization.Types as Serialization
 import Serialization.WitnessSet as Serialization.WitnessSet
 import Types.PlutusData (Datum(Datum))
-import Types.Transaction (Transaction(Transaction))
+import Types.Transaction
+  ( Transaction(Transaction)
+  , Redeemer
+  , TransactionWitnessSet
+  )
 
 data ModifyTxError
   = ConvertWitnessesError
   | ConvertDatumError
+  | ConvertRedeemerError
 
 derive instance Generic ModifyTxError _
 
@@ -35,8 +41,16 @@ attachDatum (Datum pd) (Transaction tx@{ witness_set: ws }) = runExceptT $ do
   pd' <- liftEither
     $ note ConvertDatumError
     $ Serialization.PlutusData.convertPlutusData pd
-  ws' <- liftEffect $ Serialization.WitnessSet.convertWitnessSet ws
-  liftEffect $ Serialization.WitnessSet.setPlutusData pd' ws'
-  newWits <- liftEither $ note ConvertWitnessesError
-    $ Deserialization.WitnessSet.convertWitnessSet ws'
+  newWits <- convertWitnessesWith ws $
+    Serialization.WitnessSet.setPlutusData pd'
   liftEither $ Right $ Transaction $ tx { witness_set = newWits }
+
+convertWitnessesWith
+  :: TransactionWitnessSet
+  -> (Serialization.TransactionWitnessSet -> Effect Unit)
+  -> ExceptT ModifyTxError Effect TransactionWitnessSet
+convertWitnessesWith ws act = do
+  ws' <- liftEffect $ Serialization.WitnessSet.convertWitnessSet ws
+  liftEffect $ act ws'
+  liftEither $ note ConvertWitnessesError
+    $ Deserialization.WitnessSet.convertWitnessSet ws'
