16d1270b53d070dba31f94fc48050b7b0a2b95f3
Rory Tyler Hayford <rory.hayford@protonmail.com>
2022-03-04 11:24:46 +0700

    Add issue URL to comment

--- src/QueryM.purs
+++ src/QueryM.purs
@@ -365,6 +365,7 @@ utxoQueryDispatch
   -> Effect (Either Json.JsonDecodeError (Effect Unit))
 utxoQueryDispatch ref str = do
   -- TODO: replace it with the new implementation in `Aeson`.
+  -- https://github.com/Plutonomicon/cardano-browser-tx/issues/151
   let parsed' = JsonWsp.parseJsonWspResponse =<< Helpers.parseJsonStringifyNumbers str
   case parsed' of
     (Left err) -> pure $ Left err
