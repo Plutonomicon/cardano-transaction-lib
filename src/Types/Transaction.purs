2a68c2c1c3b69722d14d2b9397f93230f2c06d9a
vvtran <viet.tran.vvt@googlemail.com>
2022-03-15 09:45:16 +0000

    clean up comment

--- src/Types/Transaction.purs
+++ src/Types/Transaction.purs
@@ -73,11 +73,11 @@ instance monoidTransaction :: Monoid Transaction where
     , auxiliary_data: Nothing
     }
 
--- Do we actually need this? Is it on chain to the point that it is not required.
+-- Do we actually need this?
 instance FromData Transaction where
   fromData = undefined
 
--- Do we actually need this? Is it on chain to the point that it is not required.
+-- Do we actually need this?
 instance ToData Transaction where
   toData = undefined
 
