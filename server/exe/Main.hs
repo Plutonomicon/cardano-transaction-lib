f73f799bfed22b29947db2cb5e1a0bf064ad3ac0
Rory Tyler Hayford <rory.hayford@protonmail.com>
2022-03-18 11:14:25 +0700

    Parse command-line port option

--- server/exe/Main.hs
+++ server/exe/Main.hs
@@ -1,10 +1,14 @@
+{-# LANGUAGE NamedFieldPuns #-}
+
 module Main (main) where
 
 import Api (app)
+import Control.Applicative ((<**>))
 import Data.Function ((&))
 import Network.HTTP.Types (Status)
 import Network.Wai (Request)
 import Network.Wai.Handler.Warp (
+  Port,
   Settings,
   defaultSettings,
   runSettings,
@@ -12,15 +16,36 @@ import Network.Wai.Handler.Warp (
   setPort,
  )
 import Network.Wai.Logger (withStdoutLogger)
+import Options.Applicative qualified as Options
 import System.Exit (die)
-import Types (newEnvIO)
+import Types (ServerOptions (ServerOptions, port), newEnvIO)
 
 main :: IO ()
-main = withStdoutLogger $ \logger ->
-  runSettings (mkSettings logger)
-    . app
-    =<< either die pure
-    =<< newEnvIO
+main = do
+  ServerOptions {port} <- Options.execParser opts
+  withStdoutLogger $ \logger ->
+    runSettings (mkSettings port logger)
+      . app
+      =<< either die pure
+      =<< newEnvIO
   where
-    mkSettings :: (Request -> Status -> Maybe Integer -> IO ()) -> Settings
-    mkSettings logger = defaultSettings & setPort 8081 & setLogger logger
+    mkSettings ::
+      Port -> (Request -> Status -> Maybe Integer -> IO ()) -> Settings
+    mkSettings port logger = defaultSettings & setPort port & setLogger logger
+
+opts :: Options.ParserInfo ServerOptions
+opts =
+  Options.info (serverOptions <**> Options.helper) $
+    Options.fullDesc <> Options.progDesc "CBTx server"
+
+serverOptions :: Options.Parser ServerOptions
+serverOptions =
+  ServerOptions
+    <$> Options.option
+      Options.auto
+      ( Options.long "port"
+          <> Options.help "Server port"
+          <> Options.showDefault
+          <> Options.value 8081
+          <> Options.metavar "INT"
+      )
