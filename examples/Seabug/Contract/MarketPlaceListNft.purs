72d569090f219e40c6913072b7e277537199010e
vvtran <viet.tran.vvt@googlemail.com>
2022-03-28 16:08:46 +0100

    fix datum to be of type MarketplaceDatum

--- examples/Seabug/Contract/MarketPlaceListNft.purs
+++ examples/Seabug/Contract/MarketPlaceListNft.purs
@@ -5,14 +5,13 @@ module Seabug.Contract.MarketPlaceListNft
 
 import Contract.Prelude
 import Contract.Address (validatorAddress)
-import Contract.Monad (Contract, liftContractE', liftedE', liftedM)
+import Contract.Monad (Contract, liftContractE', liftedM)
 import Contract.PlutusData (fromData, getDatumByHash)
 import Contract.Transaction (TransactionOutput(TransactionOutput))
 import Contract.Utxos (UtxoM, utxosAt)
-import Contract.Value (scriptCurrencySymbol, valueOf)
+import Contract.Value (valueOf)
 import Seabug.MarketPlace (marketplaceValidator)
-import Seabug.Token (mkTokenName, policy, unappliedMintingPolicy)
-import Seabug.Types (NftData(NftData))
+import Seabug.Types (MarketplaceDatum(MarketplaceDatum))
 
 -- | Lists the utxos at the script address that contain a datum of type
 -- | `NftData` with the correct NFT
@@ -23,9 +22,6 @@ marketPlaceListNft = do
     (validatorAddress marketplaceValidator'.validator)
   scriptUtxos <-
     liftedM "marketPlaceListNft: Cannot get script Utxos" (utxosAt scriptAddr)
-  -- Get the applied minting policy before hand so we don't repeat the same code
-  -- inside `containsNft`
-  mp <- liftedE' $ pure unappliedMintingPolicy
   let
     -- Given a `TxOut`/`TransactionOutput`, returns a `Boolean` on whether the
     -- datum is of type `NftData` and has the correct NFT value.
@@ -34,15 +30,9 @@ marketPlaceListNft = do
       (TransactionOutput { amount, data_hash: Just datumHash }) = do
       plutusData <- liftedM "marketPlaceListNft: Cannot get datum from hash"
         (getDatumByHash datumHash)
-      case fromData plutusData of
-        Just (NftData nftData) -> do
-          policy' <- liftedM "marketPlaceListNft: Cannot apply arguments"
-            (policy nftData.nftCollection mp)
-          curr <- liftedM "marketPlaceListNft: Cannot get CurrencySymbol"
-            (scriptCurrencySymbol policy')
-          name <- liftedM "marketPlaceListNft: Cannot hash old token"
-            (mkTokenName nftData.nftId)
-          pure $ valueOf amount curr name == one
-        _ -> pure false
+      pure case fromData plutusData of
+        Just (MarketplaceDatum { getMarketplaceDatum : curr /\ name }) ->
+          valueOf amount curr name == one
+        _ -> false
     containsNft _ = pure false
   wrap <$> filterMapM containsNft (unwrap scriptUtxos)
