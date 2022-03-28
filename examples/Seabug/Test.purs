804d4d6d6f3e725f2a3221f67fecc6e9b9a61d7b
Rory Tyler Hayford <rory.hayford@protonmail.com>
2022-03-28 15:36:01 +0700

    Log the CBOR hex of the built tx

--- examples/Seabug/Test.purs
+++ examples/Seabug/Test.purs
@@ -3,7 +3,25 @@ module Seabug.Test (main) where
 import Contract.Prelude
 
 import Contract.Monad
+import Contract.Prim.ByteArray
+import Contract.Transaction
+import Seabug.Contract.MarketPlaceBuy (mkMarketplaceTx)
+import Seabug.Types
+import Serialization as Serialization
+import Untagged.Union (asOneOf)
 
 main :: Contract Unit
 main = do
-  undefined
+  UnbalancedTx { transaction } /\ _ <- mkMarketplaceTx testNftData
+  log =<<
+    ( liftEffect
+        <<< map
+          ( byteArrayToHex
+              <<< Serialization.toBytes
+              <<< asOneOf
+          )
+    )
+      (Serialization.convertTransaction transaction)
+
+testNftData :: NftData
+testNftData = undefined
