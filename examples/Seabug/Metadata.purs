1f3610993b1834584c09e83c4ca0c66dbcd776c0
Rory Tyler Hayford <rory.hayford@protonmail.com>
2022-03-31 14:45:30 +0700

    Get the hash of the minting tx given an asset

--- examples/Seabug/Metadata.purs
+++ examples/Seabug/Metadata.purs
@@ -7,13 +7,40 @@ import Affjax.RequestBody as Affjax.RequestBody
 import Affjax.RequestHeader as Affjax.RequestHeader
 import Affjax.ResponseFormat as Affjax.ResponseFormat
 import Contract.Monad (Contract)
-import Contract.Transaction (ClientError(ClientHttpError))
+import Contract.Prim.ByteArray
+import Contract.Transaction (ClientError(ClientHttpError, ClientDecodeJsonError))
+import Contract.Value
 import Control.Monad.Reader.Trans (asks)
 import Data.Argonaut (class DecodeJson)
 import Data.Argonaut as Json
-import Data.Bifunctor (bimap)
+import Data.Bifunctor (bimap, lmap)
+import Data.Function (on)
 import Data.HTTP.Method (Method(GET))
 import Data.Newtype (unwrap)
+
+getMintingTxHash
+  :: CurrencySymbol /\ TokenName -> Contract (Either ClientError String)
+getMintingTxHash (currSym /\ tname) = mkGetRequest ("assets/" <> asset)
+  <#> either Left (decodeField "initial_mint_tx_hash")
+  where
+  asset :: String
+  asset = mkAsset (getCurrencySymbol currSym) (getTokenName tname)
+
+  mkAsset :: ByteArray -> ByteArray -> String
+  mkAsset = (<>) `on` byteArrayToHex
+
+decodeField
+  :: forall (a :: Type)
+   . DecodeJson a
+  => String
+  -> Json.Json
+  -> Either ClientError a
+decodeField field = lmap ClientDecodeJsonError <<<
+  ( Json.decodeJson
+      <=< Json.caseJsonObject
+        (Left (Json.TypeMismatch "Expected Object"))
+        (flip Json.getField field)
+  )
 
 mkGetRequest :: String -> Contract (Either ClientError Json.Json)
 mkGetRequest path = do
