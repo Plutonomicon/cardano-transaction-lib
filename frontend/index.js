dcf71ea05a09caf55a87424d955892103ebdc043
anks <michal.anks@gmail.com>
2022-04-05 18:10:06 +0200

    Remove setTimeout call in an example index.js

--- frontend/index.js
+++ frontend/index.js
@@ -5,4 +5,4 @@ const SeabugContracts = require("../seabug_contracts/api.js");
 const args = 'test';
 
 console.log("app starting");
-setTimeout(() => SeabugContracts.callMarketPlaceBuyTest(args).then(r => console.log(r)), 1);
+SeabugContracts.callMarketPlaceBuyTest(args).then(r => console.log(r));
