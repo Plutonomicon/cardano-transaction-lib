c1164b6476cb53de0327a43c37498cd4845a0e8b
Rory Tyler Hayford <rory.hayford@protonmail.com>
2022-04-21 11:05:19 +0700

    Split off integration tests

--- test/Main.purs
+++ test/Main.purs
@@ -4,12 +4,12 @@ import Prelude
 
 import Effect (Effect)
 import Effect.Aff (launchAff_)
-import Test.ByteArray as ByteArrayTest
-import Test.Parser as ParseTest
-import Test.AffInterface as AffInterface
+import Test.ByteArray as ByteArray
+import Test.Parser as Parser
 import Test.Serialization.Address as Serialization.Address
 import Test.Serialization.Hash as Serialization.Hash
 import TestM (TestPlanM)
+import Test.Integration as Integration
 import Test.Deserialization as Deserialization
 import Test.Serialization as Serialization
 import Test.Aeson as Aeson
@@ -17,9 +17,8 @@ import Test.Transaction as Transaction
 import Test.UsedTxOuts as UsedTxOuts
 import Test.Data as Data
 import Test.Metadata.Seabug as Seabug
-import Test.FinalizeTx as FinalizeTx
-import Test.Plutus.Address as PlutusAddress
 import Test.Metadata.Cip25 as Cip25
+import Test.Plutus.Address as Plutus.Address
 import Test.Utils as Utils
 
 -- we use `mote` here so that we can use effects to build up a test tree, which
@@ -27,24 +26,21 @@ import Test.Utils as Utils
 -- in Test.Spec which prohibit effects.
 main :: Effect Unit
 main = do
+  Integration.main
   launchAff_ $ Utils.interpret testPlan
 
 testPlan :: TestPlanM Unit
 testPlan = do
-  PlutusAddress.suite
+  Plutus.Address.suite
   Cip25.suite
   Data.suite
   Aeson.suite
-  ByteArrayTest.suite
-  ParseTest.suite
+  ByteArray.suite
+  Parser.suite
   Serialization.suite
   Serialization.Address.suite
   Serialization.Hash.suite
   Deserialization.suite
   Transaction.suite
   UsedTxOuts.suite
-  -- requires external servers
-  -- see README.md
-  FinalizeTx.suite
-  AffInterface.suite
   Seabug.suite
