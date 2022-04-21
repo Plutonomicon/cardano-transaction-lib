9db08766902f147f54ac908e0f826c6a2aa6c2c5
Rory Tyler Hayford <rory.hayford@protonmail.com>
2022-04-21 10:53:05 +0700

    Move `interpret` to `Test.Utils`

--- test/Main.purs
+++ test/Main.purs
@@ -2,21 +2,14 @@ module Test.Main where
 
 import Prelude
 
-import Data.Const (Const)
-import Data.Foldable (sequence_)
 import Effect (Effect)
-import Effect.Aff (Aff, launchAff_)
-import Effect.Aff.Class (liftAff)
-import Test.Spec (Spec, describe, it)
-import Test.Spec.Runner (runSpec)
-import Test.Spec.Reporter (consoleReporter)
+import Effect.Aff (launchAff_)
 import Test.ByteArray as ByteArrayTest
 import Test.Parser as ParseTest
 import Test.AffInterface as AffInterface
 import Test.Serialization.Address as Serialization.Address
 import Test.Serialization.Hash as Serialization.Hash
 import TestM (TestPlanM)
-import Mote (Plan, foldPlan, planT)
 import Test.Deserialization as Deserialization
 import Test.Serialization as Serialization
 import Test.Aeson as Aeson
@@ -27,29 +20,14 @@ import Test.Metadata.Seabug as Seabug
 import Test.FinalizeTx as FinalizeTx
 import Test.Plutus.Address as PlutusAddress
 import Test.Metadata.Cip25 as Cip25
+import Test.Utils as Utils
 
 -- we use `mote` here so that we can use effects to build up a test tree, which
 -- is then interpreted here in a pure context, mainly due to some painful types
 -- in Test.Spec which prohibit effects.
 main :: Effect Unit
 main = do
-  launchAff_ $ interpret testPlan
-
-interpret :: TestPlanM Unit -> Aff Unit
-interpret spif = do
-  plan <- planT $ spif
-  let
-    spec = go plan
-  runSpec [ consoleReporter ] spec
-  pure unit
-  where
-  go :: Plan (Const Void) (Aff Unit) -> Spec Unit
-  go =
-    foldPlan
-      (\{ label, value } -> it label $ liftAff value)
-      (\_ -> pure unit)
-      (\{ label, value } -> describe label (go $ value))
-      sequence_
+  launchAff_ $ Utils.interpret testPlan
 
 testPlan :: TestPlanM Unit
 testPlan = do
