b32e0d58105d6b9293490322acef0aff747c170d
Dzmitry Shuysky <errfrom@yandex.ru>
2022-04-28 17:33:45 +0200

    Contract.Value: Remove obsolete part of module header

--- src/Contract/Value.purs
+++ src/Contract/Value.purs
@@ -1,7 +1,4 @@
--- | A module that defines tokens in Cardano and helpers. The representation of
--- | `Value` is isomorphic to Plutus' reprsentation with the only difference
--- | being that Ada's currency symbol does not exist, instead, Ada is
--- | represented by `Coin`.
+-- | A module that defines tokens in Cardano and helpers.
 module Contract.Value
   ( module CurrencySymbol
   , module TokenName
