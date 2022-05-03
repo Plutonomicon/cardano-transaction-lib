fa1bc7b3f18d9337f824358bff0f90af99b92acd
Rory Tyler Hayford <rory.hayford@protonmail.com>
2022-05-03 14:50:57 +0700

    Add `flake-compat` back to the inputs

--- flake.nix
+++ flake.nix
@@ -2,6 +2,11 @@
   description = "cardano-transaction-lib";
 
   inputs = {
+    flake-compat = {
+      url = "github:edolstra/flake-compat";
+      flake = false;
+    };
+
     # for the purescript project
     ogmios.url = "github:mlabs-haskell/ogmios/c4f896bf32ad066be8edd8681ee11e4ab059be7f";
     ogmios-datum-cache = {
