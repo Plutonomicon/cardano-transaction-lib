2d9dac84dec7e6f849d51047efd2afedc0e64f85
Rory Tyler Hayford <rory.hayford@protonmail.com>
2022-05-05 15:46:14 +0700

    Contract paying to `AlwaysSucceeds` script address

---
+++ examples/AlwaysSucceeds.purs
@@ -0,0 +1,69 @@
+-- | This module demonstrates how the `Contract` interface can be used to build,
+-- | balance, and submit a smart-contract transaction. It creates a transaction
+-- | that pays two Ada to the `AlwaysSucceeds` script address
+-- |
+-- | * Prerequisites
+-- |   - A Chromium-based browser (for Nami compatibility)
+--
+-- |   - A Nami wallet funded with test Ada ("tAda") and collateral set, If you need
+-- |     tAda, visit https://testnets.cardano.org/en/testnets/cardano/tools/faucet/
+--
+-- | * How to run
+--
+-- |   The `Contract` interface requires several external services to be running.
+-- |   From the repository root, run `nix run .#ctl-runtime` to launch all
+-- |   required services
+--
+-- |   Once all of the services are *fully synced*, run:
+--
+-- |   - `make run-dev` and visit `localhost:4008`. You may be prompted to enable
+-- |     access to your wallet if you have not run this example before. You will
+-- |     also be prompted to sign the transaction using your Nami password
+
+module Examples.AlwaysSucceeds (main) where
+
+import Contract.Prelude
+
+import Contract.Monad
+import Contract.PlutusData (unitDatum)
+import Contract.ScriptLookups as Lookups
+import Contract.Scripts
+import Contract.Transaction
+import Contract.TxConstraints as Constraints
+import Contract.Value as Value
+import Contract.Wallet (mkNamiWalletAff)
+import Data.Argonaut (decodeJson, fromString)
+import Data.BigInt as BigInt
+import Data.Map as Map
+
+main :: Effect Unit
+main = launchAff_ $ do
+  wallet <- Just <$> mkNamiWalletAff
+  cfg <- over ContractConfig _ { wallet = wallet } <$> traceContractConfig
+  runContract_ cfg $ do
+    validator <- liftContractM "Invalid script JSON" $ alwaysSucceedsScript
+    vhash <- liftedM "Couldn't hash validator" $ validatorHash validator
+
+    let
+      -- Note that CTL does not have explicit equivalents of Plutus'
+      -- `mustPayToTheScript` or `mustPayToOtherScript`, as we have no notion
+      -- of a "current" script. Thus, we have the single constraint
+      -- `mustPayToScript`, and all scripts must be explicitly provided to build
+      -- the transaction (see the value for `lookups` below as well)
+      constraints :: Constraints.TxConstraints Void Void
+      constraints = Constraints.mustPayToScript vhash unitDatum
+        $ Value.lovelaceValueOf
+        $ BigInt.fromInt 2_000_000
+
+      lookups :: Lookups.ScriptLookups Void
+      lookups = Lookups.validator validator
+
+    ubTx <- liftedE $ Lookups.mkUnbalancedTx lookups constraints
+    BalancedSignedTransaction bsTx <-
+      liftedM "Failed to balance/sign tx" $ balanceAndSignTx ubTx
+    txId <- submit bsTx.signedTxCbor
+    logInfo' $ "Tx ID: " <> show txId
+
+alwaysSucceedsScript :: Maybe Validator
+alwaysSucceedsScript = hush $ decodeJson $ fromString
+  "4d01000033222220051200120011"
