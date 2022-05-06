1c946af7eede52ebc62d0d298f04fbc0ebfc0403
Drazen Popovic <bladyjoker@gmail.com>
2022-05-06 18:42:17 +0200

    Reviewed TypeLevel.RowList.Unordered and applied formatting

--- src/TypeLevel/RowList/RowList.purs
+++ src/TypeLevel/RowList/RowList.purs
@@ -1,9 +1,9 @@
-module TypeLevel.RowList.Unordered where
+module TypeLevel.RowList.Unordered
+  ( class AllUniqueLabels
+  ) where
 
-import TypeLevel.Nat
-import Type.RowList
+import Type.RowList (Cons, Nil, RowList)
 import Prim.TypeError (class Fail, Text)
-import Data.Symbol
 
 -- | Uniqueness constraint on the labels of an unordered RowList
 class AllUniqueLabels :: forall (k :: Type). RowList k -> Constraint
@@ -11,7 +11,7 @@ class AllUniqueLabels list
 
 instance
   Fail (Text "Labels are not unique!") =>
-  AllUniqueLabels (Cons k a (Cons k a'  xs))
+  AllUniqueLabels (Cons k a (Cons k a' xs))
 else instance AllUniqueLabels Nil
 else instance AllUniqueLabels (Cons k a Nil)
 else instance
