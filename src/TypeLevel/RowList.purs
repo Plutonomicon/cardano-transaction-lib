7d4b593d921e0998b610ab71f275e658ce779ebd
Drazen Popovic <bladyjoker@gmail.com>
2022-05-06 18:22:35 +0200

    Reviewed TypeLevel.RowList and added 'type level' tests

--- src/TypeLevel/RowList.purs
+++ src/TypeLevel/RowList.purs
@@ -1,21 +1,65 @@
-module TypeLevel.RowList where
+module TypeLevel.RowList
+  ( class AllUniqueLabels
+  , tests
+  )
+  where
 
-import TypeLevel.Nat
-import Type.RowList
 import Prim.TypeError (class Fail, Text)
-import Data.Symbol
+import Type.RowList (Cons, Nil, RowList)
 
 -- | Uniqueness constraint on the labels of an unordered RowList
 class AllUniqueLabels :: forall (k :: Type). RowList k -> Constraint
 class AllUniqueLabels list
 
+instance AllUniqueLabels Nil
+
+instance AllUniqueLabels (Cons k a Nil)
+
 instance
   Fail (Text "Labels are not unique!") =>
-  AllUniqueLabels (Cons k a (Cons k a'  xs))
-else instance AllUniqueLabels Nil
-else instance AllUniqueLabels (Cons k a Nil)
+  AllUniqueLabels (Cons k a (Cons k a' xs))
 else instance
   ( AllUniqueLabels (Cons k a xs)
   , AllUniqueLabels (Cons k' a' xs)
   ) =>
   AllUniqueLabels (Cons k a (Cons k' a' xs))
+
+-- | Poor man's type level tests
+tests ∷ Array String
+tests =
+  [ _testNil
+  , _testSingleton
+  , _testUniques
+  -- , _testDupsUnordered
+  -- , _testDups -- Should trigger 'Labels are not unique!'
+  ]
+  where
+  _testNil :: AllUniqueLabels Nil => String
+  _testNil = "Empty list has all unique labels"
+
+  _testSingleton
+    :: forall (a :: Type). AllUniqueLabels (Cons "A" a Nil) => String
+  _testSingleton = "Singleton list has all unique labels"
+
+  _testUniques
+    :: forall (a :: Type)
+     . AllUniqueLabels
+         ( Cons "A" a
+             (Cons "B" a (Cons "C" a Nil))
+         )
+    => String
+  _testUniques = "[A, B, C] is all unique and should compile"
+
+  -- TODO: Explain why the type checker triggers 'Labels are not unique'
+  _testDupsUnordered
+    :: forall (a :: Type)
+     . AllUniqueLabels (Cons "A" a (Cons "B" a (Cons "A" a (Cons "B" a Nil))))
+    => String
+  _testDupsUnordered = "[A, B, A, B] has duplicates but should compile"
+
+  _testDups
+    :: forall (a :: Type)
+     . AllUniqueLabels (Cons "A" a (Cons "A" a Nil))
+    => String
+  _testDups = "[A, A] has duplicates and shouldn't compile"
+
