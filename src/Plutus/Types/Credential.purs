2b7f56255ef5ea1d9f3e7275a797b3160154339e
Drazen Popovic <bladyjoker@gmail.com>
2022-05-19 18:01:54 +0200

    Switch Plutus.Types.Credential to use HasPlutusSchema

--- src/Plutus/Types/Credential.purs
+++ src/Plutus/Types/Credential.purs
@@ -4,15 +4,31 @@ module Plutus.Types.Credential
   ) where
 
 import Prelude
-import Data.Maybe (Maybe(Nothing))
+
+import Serialization.Address
+  ( CertificateIndex(..)
+  , Slot(..)
+  , TransactionIndex(..)
+  )
+import Data.BigInt (BigInt)
 import Data.Generic.Rep (class Generic)
+import Data.Maybe (Maybe(Nothing))
 import Data.Show.Generic (genericShow)
-import Types.Scripts (ValidatorHash)
+import FromData (class FromData, fromData, genericFromData)
+import Plutus.Types.DataSchema
+  ( class HasPlutusSchema
+  , type (:+)
+  , type (:=)
+  , type (@@)
+  , I
+  , PNil
+  )
+import Serialization.Address (Pointer)
+import ToData (class ToData, genericToData, toData)
+import TypeLevel.Nat (S, Z)
 import Types.PlutusData (PlutusData(Constr))
 import Types.PubKeyHash (PubKeyHash)
-import Serialization.Address (Pointer)
-import ToData (class ToData, toData)
-import FromData (class FromData, fromData)
+import Types.Scripts (ValidatorHash)
 
 --------------------------------------------------------------------------------
 -- Credential
@@ -36,17 +52,21 @@ derive instance Generic Credential _
 instance Show Credential where
   show = genericShow
 
+instance
+  HasPlutusSchema
+    Credential
+    ( "PubKeyCredential" := PNil @@ Z
+        :+ "ScriptCredential"
+        := PNil
+        @@ (S Z)
+        :+ PNil
+    )
+
 instance ToData Credential where
-  toData (PubKeyCredential pubKeyHash) =
-    Constr zero [ toData pubKeyHash ]
-  toData (ScriptCredential validatorHash) =
-    Constr one [ toData validatorHash ]
+  toData = genericToData
 
 instance FromData Credential where
-  fromData (Constr n [ pd ])
-    | n == zero = PubKeyCredential <$> fromData pd
-    | n == one = ScriptCredential <$> fromData pd
-  fromData _ = Nothing
+  fromData = genericFromData
 
 --------------------------------------------------------------------------------
 -- StakingCredential
@@ -57,7 +77,11 @@ instance FromData Credential where
 -- | Staking credential used to assign rewards.
 data StakingCredential
   = StakingHash Credential
-  | StakingPtr Pointer
+  | StakingPtr
+      { slot :: Slot
+      , txIx :: TransactionIndex
+      , certIx :: CertificateIndex
+      }
 
 derive instance Eq StakingCredential
 derive instance Ord StakingCredential
@@ -66,20 +90,22 @@ derive instance Generic StakingCredential _
 instance Show StakingCredential where
   show = genericShow
 
+instance
+  HasPlutusSchema
+    StakingCredential
+    ( "StakingHash" := PNil @@ Z
+        :+ "StakingPtr"
+        :=
+          ( "slot" := I Slot :+ "txIx" := I TransactionIndex :+ "certIx"
+              := I CertificateIndex
+              :+ PNil
+          )
+        @@ (S Z)
+        :+ PNil
+    )
+
 instance ToData StakingCredential where
-  toData (StakingHash credential) =
-    Constr zero [ toData credential ]
-  toData (StakingPtr ptr) =
-    Constr one [ toData ptr.slot, toData ptr.txIx, toData ptr.certIx ]
+  toData = genericToData
 
 instance FromData StakingCredential where
-  fromData (Constr n [ pd ]) | n == zero =
-    StakingHash <$> fromData pd
-  fromData (Constr n [ slotD, txIxD, certIxD ]) | n == one =
-    StakingPtr <$>
-      ( { slot: _, txIx: _, certIx: _ }
-          <$> fromData slotD
-          <*> fromData txIxD
-          <*> fromData certIxD
-      )
-  fromData _ = Nothing
+  fromData = genericFromData
