7f2c259ded5e7eb1fc599852c09fb8610bb8682b
vvtran <viet.tran.vvt@googlemail.com>
2022-06-13 12:57:53 +0100

    upgrade odc

--- flake.nix
+++ flake.nix
@@ -9,7 +9,7 @@
 
     # for the purescript project
     ogmios.url = "github:mlabs-haskell/ogmios";
-    ogmios-datum-cache.url = "github:mlabs-haskell/ogmios-datum-cache";
+    ogmios-datum-cache.url = "github:mlabs-haskell/ogmios-datum-cache/54ad2964af07ea0370bf95c0fed71f60a778ead5";
     # so named because we also need a different version of the repo below
     # in the server inputs and we use this one just for the `cardano-cli`
     # executables
