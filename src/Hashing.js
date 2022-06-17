9454b04ce5bc641dc3db5028cb943caf557995fd
Dzmitry Shuysky <errfrom@yandex.ru>
2022-06-17 17:41:09 +0200

    blake2b-wasm hashing: Throw error instead of switching to pure js impl

--- src/Hashing.js
+++ src/Hashing.js
@@ -1,8 +1,6 @@
 /* global require exports BROWSER_RUNTIME */
 
-const Blake2bPureJs = require('blakejs');
 const Blake2bWasm = require('blake2b-wasm');
-
 const SHA256 = require('jssha/dist/sha256');
 const SHA3 = require('jssha/dist/sha3');
 
@@ -13,45 +11,36 @@ if (typeof BROWSER_RUNTIME != 'undefined' && BROWSER_RUNTIME) {
   lib = require('@emurgo/cardano-serialization-lib-nodejs');
 }
 
+// -----------------------------------------------------------------------------
+// blake2b256Hash, blake2b256HashHex, hashPlutusData, hashPlutusScript
+// -----------------------------------------------------------------------------
+
 const DIGEST_LENGTH_256 = 32;
 const DIGEST_LENGTH_224 = 28;
+const DIGEST_ENCODING_BINARY = 'binary';
+const DIGEST_ENCODING_HEX = 'hex';
 
-const withBlake2bWasmObject = bytesToHash => digestLength => {
-  let obj = Blake2bWasm(digestLength);
-  obj.update(Buffer.from(bytesToHash));
-  return obj;
-};
-
-const blake2bHash = bytesToHash => digestLength => {
-  return new Promise(resolve => {
+const blake2bHash = bytesToHash => digestLength => digestEncoding => {
+  return new Promise((resolve, reject) => {
     Blake2bWasm.ready(error => {
       if (error || !Blake2bWasm.SUPPORTED) {
-        resolve(Blake2bPureJs.blake2b(bytesToHash, null, digestLength));
+        reject(new Error('Failed to calculate Blake2b hash'));
       } else {
-        resolve(withBlake2bWasmObject(bytesToHash)(digestLength).digest());
-      }
-    })
-  })
-};
-
-const blake2bHashHex = bytesToHash => digestLength => {
-  return new Promise(resolve => {
-    Blake2bWasm.ready(error => {
-      if (error || !Blake2bWasm.SUPPORTED) {
-        resolve(Blake2bPureJs.blake2bHex(bytesToHash, null, digestLength));
-      } else {
-        resolve(withBlake2bWasmObject(bytesToHash)(digestLength).digest('hex'));
+        const digest = Blake2bWasm(digestLength)
+          .update(Buffer.from(bytesToHash))
+          .digest(digestEncoding);
+        resolve(digest);
       }
     })
   })
 };
 
 exports._blake2b256Hash = bytesToHash => () => {
-  return blake2bHash(bytesToHash)(DIGEST_LENGTH_256);
+  return blake2bHash(bytesToHash)(DIGEST_LENGTH_256)(DIGEST_ENCODING_BINARY);
 };
 
 exports._blake2b256HashHex = bytesToHash => () => {
-  return blake2bHashHex(bytesToHash)(DIGEST_LENGTH_256);
+  return blake2bHash(bytesToHash)(DIGEST_LENGTH_256)(DIGEST_ENCODING_HEX);
 };
 
 exports.hashPlutusData = plutusData => {
@@ -60,13 +49,16 @@ exports.hashPlutusData = plutusData => {
 
 exports.hashPlutusScript = plutusScriptBytes => () => {
   // set Plutus language namespace byte
-  let bytes = new Uint8Array([0x1, ...plutusScriptBytes]);
-  return blake2bHash(bytes)(DIGEST_LENGTH_224);
+  const bytes = new Uint8Array([0x1, ...plutusScriptBytes]);
+  return blake2bHash(bytes)(DIGEST_LENGTH_224)(DIGEST_ENCODING_BINARY);
 };
+
+// -----------------------------------------------------------------------------
+// sha256Hash, sha256HashHex, sha3_256Hash, sha3_256HashHex
+// -----------------------------------------------------------------------------
 
 const SHA256_HASH_VARIANT = 'SHA-256';
 const SHA3_256_HASH_VARIANT = 'SHA3-256';
-
 const UINT8ARRAY_FORMAT = 'UINT8ARRAY';
 const HEX_FORMAT = 'HEX';
 
