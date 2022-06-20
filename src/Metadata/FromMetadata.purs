96924f042c9de30d718c6799c9403500169241b3
Mitch Notarnicola <mnotarni@gmail.com>
2022-06-20 18:05:10 -0400

    FromMetadata instance for Map

--- src/Metadata/FromMetadata.purs
+++ src/Metadata/FromMetadata.purs
@@ -2,16 +2,20 @@ module Metadata.FromMetadata where
 
 import Prelude
 
-import Data.Array (toUnfoldable, uncons) as Array
+import Data.Array (toUnfoldable, uncons, foldMap) as Array
 import Data.BigInt (BigInt)
+import Data.Map (Map)
+import Data.Map (fromFoldable, toUnfoldable) as Map
 import Data.Maybe (Maybe(Just, Nothing))
 import Data.NonEmpty (NonEmpty, (:|))
+import Data.Profunctor.Strong ((***))
 import Data.Traversable (traverse)
+import Data.Tuple (Tuple(..))
 import Data.Unfoldable (class Unfoldable)
 import Types.ByteArray (ByteArray)
 import Types.Int (Int, toBigInt) as Int
 import Types.TransactionMetadata
-  ( TransactionMetadatum(MetadataList, Int, Bytes, Text)
+  ( TransactionMetadatum(MetadataList, MetadataMap, Int, Bytes, Text)
   )
 
 --------------------------------------------------------------------------------
@@ -48,6 +52,14 @@ instance FromMetadata String where
   fromMetadata (Text str) = Just str
   fromMetadata _ = Nothing
 
+instance (FromMetadata k, FromMetadata v, Ord k) => FromMetadata (Map k v) where
+  fromMetadata (MetadataMap mp) =
+    Just
+      $ Map.fromFoldable
+      $ Array.foldMap fromTupleMaybe
+      $ (fromMetadata *** fromMetadata) <$> (Map.toUnfoldable mp)
+  fromMetadata _ = Nothing
+
 --------------------------------------------------------------------------------
 -- Internal
 --------------------------------------------------------------------------------
@@ -61,3 +73,10 @@ fromMetadataUnfoldable
 fromMetadataUnfoldable (MetadataList entries) =
   Array.toUnfoldable <$> traverse fromMetadata entries
 fromMetadataUnfoldable _ = Nothing
+
+fromTupleMaybe
+  :: forall (a :: Type) (b :: Type)
+   . Tuple (Maybe a) (Maybe b)
+  -> Array (Tuple a b)
+fromTupleMaybe (Tuple (Just a) (Just b)) = [ Tuple a b ]
+fromTupleMaybe (Tuple _ _) = []
