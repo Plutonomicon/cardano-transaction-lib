25df6bc87a95a6e1342c204333e6ba6b260f4352
Mike Hartl <mikehartl17@gmail.com>
2022-06-27 12:08:49 +0200

    Sign Nami transaction

--- test/Examples/Pkh2Pkh.purs
+++ test/Examples/Pkh2Pkh.purs
@@ -14,10 +14,10 @@ import Control.Monad.Error.Class (catchError)
 import Data.Array (head, filter, zip)
 import Data.Functor (void)
 import Data.Maybe (Maybe(..), isJust, fromMaybe)
-import Data.Newtype (class Newtype, wrap)
+import Data.Newtype (class Newtype, wrap, unwrap)
 import Data.Tuple (fst, snd)
-import Effect.Class.Console (log)
 import Effect.Class (liftEffect)
+import Effect.Class.Console (log)
 import Effect.Exception (throw)
 import Foreign (Foreign)
 import Foreign as Foreign
@@ -65,17 +65,71 @@ derive instance Newtype NoShowPage _
 instance Show NoShowPage where
   show _ = "Toppoki.Page"
 
-clickButton :: String -> Toki.Page -> Aff (Maybe Unit)
-clickButton buttonText page = do
-  let selector = "button"
-  log selector
-  tryJs (wrap selector) "(b) => b.click()" page
+newtype Selector = Selector String
+derive instance Newtype Selector _
+
+newtype Action = Action String
+derive instance Newtype Action _
+
+doJQ :: Selector -> Action -> Toki.Page -> Aff Unit
+doJQ selector action page = do
+  log jq
+  void $ Toki.unsafeEvaluateStringFunction jq page
+  where jq :: String
+        jq = "$('" <> unwrap selector <> "')." <> unwrap action
+
+click :: Action
+click = wrap "click()"
+
+enable :: Action
+enable = wrap "prop(\"disabled\", false)"
+
+setAttr :: String -> String -> Action
+setAttr attr value = wrap $ "attr(" <> jqStr attr <> ", " <> value <> ")"
+
+buttonWithText :: String -> Selector
+buttonWithText text = wrap $ "button:contains(" <> text <> ")"
+
+password :: Selector
+password = wrap ":password"
+
+jqStr :: String -> String
+jqStr str = "\"" <> str <> "\""
+
+jqSet :: String -> String -> Action
+jqSet what value = wrap $ what <> "(" <> value <> ")"
+
+setText :: String -> Action
+setText = jqSet "text" <<< jqStr
+
+setValue :: String -> Action
+setValue = jqSet "val" <<< jqStr
+
+trigger :: String -> Action
+trigger event = wrap $ "trigger(" <> jqStr event <> ")"
+
+clickButton :: String -> Toki.Page -> Aff Unit
+clickButton buttonText = doJQ (buttonWithText buttonText) click
 
 injectJQueryAll :: String -> Toki.Browser -> Aff Unit
 injectJQueryAll jQuery browser = do
   pages <- Toki.pages browser
   void $ for pages $ Toki.unsafeEvaluateStringFunction jQuery
-  
+
+testPassword :: String
+testPassword = "ctlctlctl"
+
+reactSetValue :: Selector -> String -> Toki.Page -> Aff Unit
+reactSetValue selector value page = void $ flip Toki.unsafeEvaluateStringFunction page $ fold
+                     [ -- https://stackoverflow.com/questions/23892547/what-is-the-best-way-to-trigger-onchange-event-in-react-js
+                       -- Nami uses react, which complicates things a bit
+                       "var input = $('" <> unwrap selector <> "').get(0);"
+                     , "var nativeInputValueSetter = Object.getOwnPropertyDescriptor(window.HTMLInputElement.prototype, 'value').set;"
+                     , "nativeInputValueSetter.call(input, '" <> value <> "');"
+                     , "var ev2 = new Event('input', { bubbles: true});"
+                     , "input.dispatchEvent(ev2);"
+                     ]
+
 x :: Aff Unit
 x = do
   browser <- launchWithNami Visible
@@ -88,8 +142,11 @@ x = do
   shouldSatisfy (NoShowPage <$> namiPage) isJust
   case namiPage of
     Nothing -> liftEffect $ throw "Impossible"
-    Just np -> void $ Toki.unsafeEvaluateStringFunction "$('button:contains(Sign)').click()" np
-  delay (wrap 60000.0)
+    Just np -> do clickButton "Access" np -- this matches when the wallet is used the first time
+                  clickButton "Sign" np
+                  reactSetValue password testPassword np
+                  clickButton "Confirm" np
+  delay (wrap 600000.0)
   
 {-  let namiPage' :: Toki.Page
       namiPage' = fromMaybe page namiPage
