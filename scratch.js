9915a9ef7c6d0596b533f57897fc3ffad460cdfc
Rory Tyler Hayford <rory.hayford@protonmail.com>
2022-07-08 16:27:32 +0700

    Remove old module

--- scratch.js
+++
@@ -1,5 +0,0 @@
-const Ogmios = require("./output/Ogmios");
-
-const ws = Ogmios.setupConnectionAndQuery(
-  "addr_test1qr7g8nrv76fc7k4ueqwecljxx9jfwvsgawhl55hck3n8uwaz26mpcwu58zdkhpdnc6nuq3fa8vylc8ak9qvns7r2dsysp7ll4d"
-)();
