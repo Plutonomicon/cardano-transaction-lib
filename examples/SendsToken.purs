a186e228d17bd25b759ea403b9831b1c31a88f08
Dzmitry Shuysky <errfrom@yandex.ru>
2022-08-23 16:08:38 +0200

    Add SendsToken example that reproduces convertValue error

---
+++ examples/SendsToken.purs
@@ -0,0 +1,79 @@
+module Examples.SendsToken (main, example, contract) where
+
+import Contract.Prelude
+
+import Contract.Address (ownPaymentPubKeyHash, ownStakePubKeyHash)
+import Contract.Config (ConfigParams, testnetNamiConfig)
+import Contract.Log (logInfo')
+import Contract.Monad
+  ( Contract
+  , launchAff_
+  , liftContractAffM
+  , liftedM
+  , runContract
+  )
+import Contract.ScriptLookups as Lookups
+import Contract.Scripts (MintingPolicy)
+import Contract.Test.E2E (publishTestFeedback)
+import Contract.Transaction (TransactionHash, awaitTxConfirmed)
+import Contract.TxConstraints as Constraints
+import Contract.Value (Value)
+import Contract.Value as Value
+import Contract.Wallet (KeyWallet, withKeyWallet)
+import Examples.AlwaysMints (alwaysMintsPolicy)
+import Examples.Helpers
+  ( buildBalanceSignAndSubmitTx
+  , mkCurrencySymbol
+  , mkTokenName
+  , mustPayToPubKeyStakeAddress
+  ) as Helpers
+
+main :: Effect Unit
+main = example testnetNamiConfig
+
+example :: ConfigParams () -> Effect Unit
+example cfg = launchAff_ do
+  runContract cfg contract
+  publishTestFeedback true
+
+contract :: Contract () Unit
+contract = do
+  logInfo' "Running Examples.SendsToken"
+
+  mintToken >>= awaitTxConfirmed
+  logInfo' "Tx submitted successfully, Sending token to own address"
+
+  sendToken >>= awaitTxConfirmed
+  logInfo' "Tx submitted successfully!"
+
+mintToken :: Contract () TransactionHash
+mintToken = do
+  mp /\ value <- tokenValue
+  let
+    constraints :: Constraints.TxConstraints Void Void
+    constraints = Constraints.mustMintValue value
+
+    lookups :: Lookups.ScriptLookups Void
+    lookups = Lookups.mintingPolicy mp
+
+  Helpers.buildBalanceSignAndSubmitTx lookups constraints
+
+sendToken :: Contract () TransactionHash
+sendToken = do
+  pkh <- liftedM "Failed to get own PKH" ownPaymentPubKeyHash
+  skh <- ownStakePubKeyHash
+  _ /\ value <- tokenValue
+  let
+    constraints :: Constraints.TxConstraints Void Void
+    constraints = Helpers.mustPayToPubKeyStakeAddress pkh skh value
+
+    lookups :: Lookups.ScriptLookups Void
+    lookups = mempty
+
+  Helpers.buildBalanceSignAndSubmitTx lookups constraints
+
+tokenValue :: Contract () (MintingPolicy /\ Value)
+tokenValue = do
+  mp /\ cs <- Helpers.mkCurrencySymbol alwaysMintsPolicy
+  tn <- Helpers.mkTokenName "TheToken"
+  pure $ mp /\ Value.singleton cs tn one
