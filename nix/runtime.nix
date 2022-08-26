142035865db2345e6aad9d2d9597ac8b212968f0
Vladimir Kalnitsky <vladimir@mlabs.city>
2022-08-26 22:27:06 +0400

    Use valid firstBlock config for preprod

--- nix/runtime.nix
+++ nix/runtime.nix
@@ -29,8 +29,8 @@ rec {
       controlApiToken = "";
       blockFetcher = {
         firstBlock = {
-          slot = 61625527;
-          id = "3afd8895c7b270f8250b744ec8d2b3c53ee2859c9d5711d906c47fe51b800988";
+          slot = 5854109;
+          id = "85366c607a9777b887733de621aa2008aec9db4f3e6a114fb90ec2909bc06f14";
         };
         autoStart = true;
         startFromLast = false;
