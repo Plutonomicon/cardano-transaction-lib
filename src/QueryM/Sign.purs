5d8e4fc737730f9ae86f61dd71fa96d6ead01286
Vladimir Kalnitsky <vladimir@mlabs.city>
2022-09-16 15:10:38 +0400

    Refactor QueryM.Sign module.

--- src/QueryM/Sign.purs
+++ src/QueryM/Sign.purs
@@ -4,7 +4,7 @@ module QueryM.Sign
 
 import Prelude
 
-import Cardano.Types.Transaction (UtxoMap, _witnessSet, _body, _inputs)
+import Cardano.Types.Transaction (_body, _inputs, _witnessSet)
 import Cardano.Types.Transaction as Transaction
 import Data.Array (elem, fromFoldable)
 import Data.Lens ((<>~))
@@ -34,52 +34,52 @@ signTransaction tx = withMWallet case _ of
     let
       txInputs :: Array TransactionInput
       txInputs = fromFoldable $ tx ^. _body <<< _inputs
-
-    ownAddrs <- liftedM (error "Could not get addresses") getWalletAddresses
-    ownInputUtxos <- txInputs #
-      traverse
-        ( \txInput -> do
-            utxo <- liftedM (error "Could not get utxo") $ getUtxo txInput
-            pure (txInput /\ utxo)
-        ) >>> map
-        ( Map.fromFoldable >>> Map.filter
-            ( flip elem ownAddrs
-                <<< _.address
-                <<< unwrap
-            )
-        )
-    delayUntilUtxosSynchronized ownInputUtxos
+    walletWaitForInputs txInputs
     liftAff $ callCip30Wallet eternl \nw -> flip nw.signTx tx
   Lode lode -> liftAff $ callCip30Wallet lode \nw -> flip nw.signTx tx
   KeyWallet kw -> liftAff do
     witnessSet <- (unwrap kw).signTx tx
     pure $ Just (tx # _witnessSet <>~ witnessSet)
 
--- | Waits till all UTxOs in a given map appear in the UTxOs provided by wallet.
+-- | Waits till all provided inputs of a given transaction appear in the UTxO
+-- | set provided by the wallet.
 -- | This is a hacky solution to the problem of Eternl not seeing UTxOs that
 -- | hasn't been fully confirmed at the moment of a `sign()` call.
--- | Since it can't detect their origin, it can't know which of the private keys
--- | to use for signing. As a result, we get `MissingVKeyWitnesses`.
-delayUntilUtxosSynchronized :: UtxoMap -> QueryM Unit
-delayUntilUtxosSynchronized ownInputUtxos =
+-- | Since it can't detect UTxO origin, it can't decide which of the private
+-- | keys to use for signing. As a result, we get `MissingVKeyWitnesses`.
+walletWaitForInputs :: Array TransactionInput -> QueryM Unit
+walletWaitForInputs txInputs = do
+  ownAddrs <- liftedM (error "Could not get addresses") getWalletAddresses
+  ownInputUtxos <- txInputs #
+    traverse
+      ( \txInput -> do
+          utxo <- liftedM (error "Could not get utxo") $ getUtxo txInput
+          pure (txInput /\ utxo)
+      ) >>> map
+      ( Map.fromFoldable >>> Map.filter
+          ( flip elem ownAddrs
+              <<< _.address
+              <<< unwrap
+          )
+      )
+  let
+    go attempts = do
+      walletUtxos <- getWalletUtxos <#> fromMaybe Map.empty
+      unless (ownInputUtxos `Map.isSubmap` walletUtxos) do
+        when (attempts == 0) do
+          liftEffect $ throw $
+            "walletWaitForInputs: timeout while waiting for wallet"
+              <> " UTxO set and CTL query layer UTxO set to synchronize. UTxOs"
+              <> " from Ogmios: "
+              <> show ownInputUtxos
+              <> ", UTxOs from wallet: "
+              <> show walletUtxos
+              <> ", UTxOs that didn't appear in the wallet: "
+              <>
+                show (Map.difference ownInputUtxos walletUtxos)
+        liftAff $ delay $ wrap $ 1000.0
+        go (attempts - 1)
   -- As clarified in Eternl discord, they synchronize with the server every 2
   -- minutes, so 150 seconds would probably be enough to also account for
   -- possible network latency.
   go 150
-  where
-  go attempts = do
-    walletUtxos <- getWalletUtxos <#> fromMaybe Map.empty
-    unless (ownInputUtxos `Map.isSubmap` walletUtxos) do
-      liftAff $ delay $ wrap $ 1000.0
-      when (attempts == 0) do
-        liftEffect $ throw $
-          "delayUntilUtxosSynchronized: timeout while waiting for wallet"
-            <> " UTxO set and CTL query layer UTxO set to synchronize. UTxOs"
-            <> " from Ogmios: "
-            <> show ownInputUtxos
-            <> ", UTxOs from wallet: "
-            <> show walletUtxos
-            <> ", UTxOs that didn't appear in the wallet: "
-            <>
-              show (Map.difference ownInputUtxos walletUtxos)
-      go (attempts - 1)
