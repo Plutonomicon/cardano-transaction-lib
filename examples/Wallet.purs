14f8c67cec081a48bf0d3198f9cdf63dbea68c5a
Amir Rad <ahmr10@gmail.com>
2022-09-19 20:44:11 +0100

    Remove unused imports

--- examples/Wallet.purs
+++ examples/Wallet.purs
@@ -2,17 +2,11 @@ module Examples.Wallet (example, contract) where
 
 import Contract.Prelude
 
-import Cardano.Types.Transaction (PublicKey)
 import Contract.Address (getWalletAddress, getWalletCollateral)
 import Contract.Config (ConfigParams)
-import Contract.Log (logDebug')
 import Contract.Monad (Contract, launchAff_, runContract)
 import Contract.Test.E2E (publishTestFeedback)
-import Contract.Transaction (mkPubKey)
 import Contract.Utxos (getWalletBalance, getWalletUtxos)
-import FromData (fromData)
-import Partial.Unsafe (unsafePartial)
-import ToData (toData)
 
 contract :: Contract () Unit
 contract = do
