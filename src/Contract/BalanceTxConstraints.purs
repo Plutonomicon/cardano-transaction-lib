106fd01cc7ae5736f6eebebe58e09fc931867569
Dzmitry Shuysky <errfrom@yandex.ru>
2022-09-23 16:34:21 +0200

    Add doc comments for balancer constraints interface

--- src/Contract/BalanceTxConstraints.purs
+++ src/Contract/BalanceTxConstraints.purs
@@ -1,3 +1,5 @@
+-- | A module that defines several balancer constraints that can be used to 
+-- | adjust the behaviour of the balancer.
 module Contract.BalanceTxConstraints (module BalanceTxConstraints) where
 
 import BalanceTx.Constraints
