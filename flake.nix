84c2991d8fdaacaa742af2a71a5012b2d663b3d2
Luis Alberto Diaz Diaz <73986926+Luis-omega@users.noreply.github.com>
2022-09-22 19:38:57 -0500

    updated ODC hash

--- flake.nix
+++ flake.nix
@@ -17,11 +17,11 @@
     };
 
     plutip.url = "github:mlabs-haskell/plutip/8364c43ac6bc9ea140412af9a23c691adf67a18b";
-    ogmios-datum-cache.url = "github:mlabs-haskell/ogmios-datum-cache/ff989fbfab09e2ede804ad9903c144e92369c8a6";
-    # Repository with network parameters
-    cardano-configurations = {
-      # Override with "path:/path/to/cardano-configurations";
-      url = "github:input-output-hk/cardano-configurations";
+    ogmios-datum-cache.url = "github:mlabs-haskell/ogmios-datum-cache/ada4d2efdf7c4f308835099d0d30a91c1bd4a565";
+    # Repository with network parameters                                
+    cardano-configurations = {                                          
+      # Override with "path:/path/to/cardano-configurations";           
+      url = "github:input-output-hk/cardano-configurations";            
       flake = false;
     };
     easy-purescript-nix = {
