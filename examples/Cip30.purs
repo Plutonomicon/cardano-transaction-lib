17a72a7da5c6d9d0dce5645c1d2163d4994a93c4
Luis Alberto Díaz Díaz <73986926+Luis-omega@users.noreply.github.com>
2022-09-28 23:02:02 -0500

    added KeyWallet to examples.Cip30

--- examples/Cip30.purs
+++ examples/Cip30.purs
@@ -5,15 +5,18 @@ module Ctl.Examples.Cip30
   (main
   ) where
 
+
 import Contract.Prelude
 
 import Contract.Config (ConfigParams, testnetNamiConfig)
 import Contract.Log (logInfo')
-import Contract.Monad (Contract, launchAff_, liftContractAffM, runContract)
+import Contract.Monad (Contract, launchAff_, liftContractAffM)
 import Contract.Test.E2E (publishTestFeedback)
 import Contract.Wallet (getChangeAddress, getNetworkId, getRewardAddresses, getUnusedAddresses, signData)
 import Control.Monad.Error.Class (liftMaybe)
+import Ctl.Examples.KeyWallet.Internal.Pkh2PkhContract (runKeyWalletContract_)
 import Ctl.Internal.Types.RawBytes (hexToRawBytes)
+import Effect.Aff (try)
 import Effect.Exception (error)
 
 
@@ -23,28 +26,30 @@ main = example testnetNamiConfig
 example :: ConfigParams () -> Effect Unit
 example cfg = launchAff_ do
   runContract cfg contract
+  liftEffect $ runKeyWalletContract_ (\ _ _ _ -> contract true)
   publishTestFeedback true
 
-contract :: Contract () Unit
-contract = do
+contract :: Boolean -> Contract () Unit
+contract catch = do
   logInfo' "Running Examples.Cip30"
-  performAndLog "getNetworkId" getNetworkId
-  performAndLog "getUnusedAddresses" getUnusedAddresses
-  performAndLog "getChangeAddress" getChangeAddress
-  performAndLog "getRewardAddresses" getRewardAddresses
+  performAndLog catch "getNetworkId" getNetworkId 
+  performAndLog catch "getUnusedAddresses" getUnusedAddresses
+  performAndLog false "getChangeAddress" getChangeAddress
+  performAndLog false "getRewardAddresses" getRewardAddresses
   maddress <-  getChangeAddress
   address <- liftMaybe (error "can't get change address") maddress
   dataBytes <- liftContractAffM
     ("can't convert : " <> hexDataString <>" to RawBytes") 
     (pure mDataBytes)
-  performAndLog "dataAddress" $ signData address dataBytes
+  performAndLog  catch "signData" $ signData address dataBytes
   where 
   hexDataString = "aeff"
   mDataBytes = hexToRawBytes hexDataString 
 
-performAndLog :: forall (a::Type) . Show a => String -> 
+
+performAndLog :: forall (a::Type) . Show a => Boolean -> String ->
   Contract () a -> Contract () Unit 
-performAndLog msg cont = do 
-  result <- cont
+performAndLog catch msg cont = do 
+  result <- if catch then try cont else pure <$> cont
   logInfo' $ msg <> ": " <> show result
 
