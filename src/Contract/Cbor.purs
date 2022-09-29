43eda6d48b9c1f878a8e37208ccb4efc47a39d5a
Mitch Notarnicola <mnotarni@gmail.com>
2022-09-29 10:07:17 -0400

    Add `cborToHex`

--- src/Contract/Cbor.purs
+++ src/Contract/Cbor.purs
@@ -1,16 +1,22 @@
+-- | A module with CBOR-related functionality.
 module Contract.Cbor
-  ( module CborBytes
+  ( module Ctl.Internal.Types.Cbor
+  , cborToHex
   , hexToCbor
   ) where
 
 import Contract.Prelude
 
 import Ctl.Internal.Types.Cbor (Cbor)
-import Ctl.Internal.Types.CborBytes (hexToCborBytes) as CborBytes
-import Data.Newtype (wrap)
+import Ctl.Internal.Types.CborBytes (cborBytesToHex, hexToCborBytes)
+import Data.Newtype (unwrap, wrap)
 
 -- | Decode a hexadecimal string into a `Cbor`.
 -- | Results in `Nothing` if the string contains a non-hexadecimal number,
 -- | or if the string is not even in length.
 hexToCbor :: String -> Maybe Cbor
-hexToCbor = map wrap <<< CborBytes.hexToCborBytes
+hexToCbor = map wrap <<< hexToCborBytes
+
+-- | Encode a `Cbor` into a hexadecimal string.
+cborToHex :: Cbor -> String
+cborToHex = cborBytesToHex <<< unwrap
