8681a0decce59952b38d70c7eb91d82ce61cd1c6
peter <peter.bruehwiler@gmail.com>
2022-10-06 15:24:10 +0200

    include in spending transaction

--- examples/IncludeDatum.js
+++ examples/IncludeDatum.js
@@ -2,14 +2,14 @@
 
 let script;
 if (typeof BROWSER_RUNTIME != "undefined" && BROWSER_RUNTIME) {
-    script = require("Scripts/include-datum.plutus");
+  script = require("Scripts/include-datum.plutus");
 } else {
-    const fs = require("fs");
-    const path = require("path");
-    script = fs.readFileSync(
-        path.resolve(__dirname, "../../fixtures/scripts/include-datum.plutus"),
-        "utf8"
-    );
+  const fs = require("fs");
+  const path = require("path");
+  script = fs.readFileSync(
+    path.resolve(__dirname, "../../fixtures/scripts/include-datum.plutus"),
+    "utf8"
+  );
 }
 
 exports.includeDatum = script;
