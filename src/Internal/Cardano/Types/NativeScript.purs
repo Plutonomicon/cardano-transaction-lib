364570c2197a21b980456f51fabd4c15f90c62ca
Amir Rad <ahmr10@gmail.com>
2022-10-06 19:22:12 +0100

    Add roundtrip property test for native script aeson encode/decdoe

--- src/Internal/Cardano/Types/NativeScript.purs
+++ src/Internal/Cardano/Types/NativeScript.purs
@@ -24,10 +24,19 @@ import Aeson.Decode.Decoders (decodeArray)
 import Ctl.Internal.Helpers (encodeTagged')
 import Ctl.Internal.Metadata.Helpers (errExpectedObject)
 import Ctl.Internal.Serialization.Address (Slot)
-import Ctl.Internal.Serialization.Hash (Ed25519KeyHash)
+import Ctl.Internal.Serialization.Hash (Ed25519KeyHash, ed25519KeyHashFromBytes)
+import Ctl.Internal.Types.BigNum (fromString)
+import Ctl.Internal.Types.RawBytes (hexToRawBytesUnsafe)
+import Data.Array.NonEmpty (fromFoldable)
 import Data.Either (Either(Left))
 import Data.Generic.Rep (class Generic)
+import Data.Maybe (fromJust)
+import Data.Newtype (wrap)
 import Data.Show.Generic (genericShow)
+import Partial.Unsafe (unsafePartial)
+import Test.QuickCheck (class Arbitrary)
+import Test.QuickCheck.Arbitrary (arbitrary)
+import Test.QuickCheck.Gen (Gen, oneOf, resize, sized, suchThat)
 
 data NativeScript
   = ScriptPubkey Ed25519KeyHash
@@ -42,6 +51,25 @@ derive instance Generic NativeScript _
 
 instance Show NativeScript where
   show x = genericShow x
+
+instance Arbitrary NativeScript where
+  arbitrary = oneOf $ unsafePartial $ fromJust $ fromFoldable
+    [ ScriptPubkey <$> (pure pk)
+    , ScriptAll <$> sized (\i -> resize (i `div` 2) arbitrary)
+    , ScriptAny <$> sized (\i -> resize (i `div` 2) arbitrary)
+    , ScriptNOfK <$> arbitrary <*> sized (\i -> resize (i `div` 2) arbitrary)
+    , TimelockStart <$> map
+        (wrap <<< (unsafePartial $ fromJust <<< fromString <<< show))
+        (suchThat (arbitrary :: Gen Int) (_ > 0))
+    , TimelockExpiry <$> map
+        (wrap <<< (unsafePartial $ fromJust <<< fromString <<< show))
+        (suchThat (arbitrary :: Gen Int) (_ > 0))
+    ]
+    where
+    pk :: Ed25519KeyHash
+    pk = unsafePartial $ fromJust $ ed25519KeyHashFromBytes $
+      hexToRawBytesUnsafe
+        "1c12f03c1ef2e935acc35ec2e6f96c650fd3bfba3e96550504d53361"
 
 instance DecodeAeson NativeScript where
   decodeAeson = caseAesonObject errExpectedObject $ \obj -> do
