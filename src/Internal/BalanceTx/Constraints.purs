1e7fae52e96b1ec770cca9d30110cdbf736dd599
Dzmitry Shuysky <errfrom@yandex.ru>
2022-10-07 15:50:19 +0200

    Balancer constraints: Comment out additionalUtxos related code

--- src/Internal/BalanceTx/Constraints.purs
+++ src/Internal/BalanceTx/Constraints.purs
@@ -7,8 +7,8 @@ module Ctl.Internal.BalanceTx.Constraints
   , mustGenChangeOutsWithMaxTokenQuantity
   , mustNotSpendUtxosWithOutRefs
   , mustNotSpendUtxoWithOutRef
-  , mustUseAdditionalUtxos
-  , _additionalUtxos
+  -- , mustUseAdditionalUtxos
+  -- , _additionalUtxos
   , _maxChangeOutputTokenQuantity
   , _nonSpendableInputs
   , _ownAddresses
@@ -21,7 +21,7 @@ import Ctl.Internal.Plutus.Types.Address
   ( Address
   , AddressWithNetworkTag(AddressWithNetworkTag)
   ) as Plutus
-import Ctl.Internal.Plutus.Types.Transaction (UtxoMap) as Plutus
+-- import Ctl.Internal.Plutus.Types.Transaction (UtxoMap) as Plutus
 import Ctl.Internal.Serialization.Address (Address, NetworkId)
 import Ctl.Internal.Types.Transaction (TransactionInput)
 import Data.Array (singleton) as Array
@@ -30,8 +30,8 @@ import Data.Function (applyFlipped)
 import Data.Lens (Lens')
 import Data.Lens.Iso.Newtype (_Newtype)
 import Data.Lens.Record (prop)
-import Data.Lens.Setter (appendOver, set, setJust)
-import Data.Map (empty) as Map
+import Data.Lens.Setter (appendOver, setJust) -- set
+-- import Data.Map (empty) as Map
 import Data.Maybe (Maybe(Nothing))
 import Data.Newtype (class Newtype, over2, unwrap, wrap)
 import Data.Set (Set)
@@ -39,16 +39,16 @@ import Data.Set (singleton) as Set
 import Type.Proxy (Proxy(Proxy))
 
 newtype BalanceTxConstraints = BalanceTxConstraints
-  { additionalUtxos :: Plutus.UtxoMap
-  , maxChangeOutputTokenQuantity :: Maybe BigInt
+  -- { additionalUtxos :: Plutus.UtxoMap
+  { maxChangeOutputTokenQuantity :: Maybe BigInt
   , nonSpendableInputs :: Set TransactionInput
   , ownAddresses :: Maybe (Array Address)
   }
 
 derive instance Newtype BalanceTxConstraints _
 
-_additionalUtxos :: Lens' BalanceTxConstraints Plutus.UtxoMap
-_additionalUtxos = _Newtype <<< prop (Proxy :: Proxy "additionalUtxos")
+-- _additionalUtxos :: Lens' BalanceTxConstraints Plutus.UtxoMap
+-- _additionalUtxos = _Newtype <<< prop (Proxy :: Proxy "additionalUtxos")
 
 _maxChangeOutputTokenQuantity :: Lens' BalanceTxConstraints (Maybe BigInt)
 _maxChangeOutputTokenQuantity =
@@ -76,8 +76,8 @@ buildBalanceTxConstraints = applyFlipped defaultConstraints <<< unwrap
   where
   defaultConstraints :: BalanceTxConstraints
   defaultConstraints = wrap
-    { additionalUtxos: Map.empty
-    , maxChangeOutputTokenQuantity: Nothing
+    -- { additionalUtxos: Map.empty
+    { maxChangeOutputTokenQuantity: Nothing
     , nonSpendableInputs: mempty
     , ownAddresses: Nothing
     }
@@ -115,6 +115,5 @@ mustNotSpendUtxoWithOutRef = mustNotSpendUtxosWithOutRefs <<< Set.singleton
 
 -- | Tells the balancer to use the provided utxo set when evaluating script 
 -- | execution units (Sets `additionalUtxoSet` of Ogmios `EvaluateTx`).
-mustUseAdditionalUtxos :: Plutus.UtxoMap -> BalanceTxConstraintsBuilder
-mustUseAdditionalUtxos = wrap <<< set _additionalUtxos
-
+-- mustUseAdditionalUtxos :: Plutus.UtxoMap -> BalanceTxConstraintsBuilder
+-- mustUseAdditionalUtxos = wrap <<< set _additionalUtxos
