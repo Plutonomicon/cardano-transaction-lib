cfeccc99d571e8be6acb32d55b424f5782397b71
peter <peter.bruehwiler@gmail.com>
2022-10-13 10:01:19 +0200

    add BigNum functions to Contract.Numeric.BigNum

---
+++ src/Contract/Numeric/BigNum.purs
@@ -0,0 +1,15 @@
+module Contract.Numeric.BigNum (module X) where
+
+import Ctl.Internal.Types.BigNum
+  ( BigNum
+  , add
+  , fromBigInt
+  , fromInt
+  , fromString
+  , fromStringUnsafe
+  , maxValue
+  , mul
+  , one
+  , fromUInt
+  , zero
+  ) as X
