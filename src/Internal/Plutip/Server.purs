9bbb11a9fcfc644aa3fe34080eda84788fb484ae
peter <peter.bruehwiler@gmail.com>
2022-10-27 17:59:41 +0200

    cleanup

--- src/Internal/Plutip/Server.purs
+++ src/Internal/Plutip/Server.purs
@@ -15,13 +15,44 @@ import Affjax.RequestBody as RequestBody
 import Affjax.RequestHeader as Header
 import Affjax.ResponseFormat as Affjax.ResponseFormat
 import Contract.Address (NetworkId(MainnetId))
-import Contract.Monad (Contract, ContractEnv(ContractEnv), liftContractM, runContractInEnv)
+import Contract.Monad
+  ( Contract
+  , ContractEnv(ContractEnv)
+  , liftContractM
+  , runContractInEnv
+  )
 import Ctl.Internal.Plutip.PortCheck (isPortAvailable)
-import Ctl.Internal.Plutip.Spawn (NewOutputAction(Success, NoOp), killOnExit, spawnAndWaitForOutput)
-import Ctl.Internal.Plutip.Types (ClusterStartupParameters, ClusterStartupRequest(ClusterStartupRequest), InitialUTxODistribution, InitialUTxOs, PlutipConfig, PostgresConfig, PrivateKeyResponse(PrivateKeyResponse), StartClusterResponse(ClusterStartupSuccess, ClusterStartupFailure), StopClusterRequest(StopClusterRequest), StopClusterResponse)
+import Ctl.Internal.Plutip.Spawn
+  ( NewOutputAction(Success, NoOp)
+  , killOnExit
+  , spawnAndWaitForOutput
+  )
+import Ctl.Internal.Plutip.Types
+  ( ClusterStartupParameters
+  , ClusterStartupRequest(ClusterStartupRequest)
+  , InitialUTxODistribution
+  , InitialUTxOs
+  , PlutipConfig
+  , PostgresConfig
+  , PrivateKeyResponse(PrivateKeyResponse)
+  , StartClusterResponse(ClusterStartupSuccess, ClusterStartupFailure)
+  , StopClusterRequest(StopClusterRequest)
+  , StopClusterResponse
+  )
 import Ctl.Internal.Plutip.Utils (tmpdir)
-import Ctl.Internal.Plutip.UtxoDistribution (class UtxoDistribution, decodeWallets, encodeDistribution, keyWallets, transferFundsFromEnterpriseToBase)
-import Ctl.Internal.QueryM (ClientError(ClientDecodeJsonError, ClientHttpError), Logger, mkLogger, stopQueryRuntime)
+import Ctl.Internal.Plutip.UtxoDistribution
+  ( class UtxoDistribution
+  , decodeWallets
+  , encodeDistribution
+  , keyWallets
+  , transferFundsFromEnterpriseToBase
+  )
+import Ctl.Internal.QueryM
+  ( ClientError(ClientDecodeJsonError, ClientHttpError)
+  , Logger
+  , mkLogger
+  , stopQueryRuntime
+  )
 import Ctl.Internal.QueryM as QueryM
 import Ctl.Internal.QueryM.Logging (setupLogs)
 import Ctl.Internal.QueryM.ProtocolParameters as Ogmios
@@ -48,10 +79,23 @@ import Data.UInt as UInt
 import Effect (Effect)
 import Effect.Aff (Aff, Milliseconds(Milliseconds), bracket, throwError, try)
 import Effect.Aff.Class (liftAff)
-import Effect.Aff.Retry (RetryPolicy, constantDelay, limitRetriesByCumulativeDelay, recovering)
+import Effect.Aff.Retry
+  ( RetryPolicy
+  , constantDelay
+  , limitRetriesByCumulativeDelay
+  , recovering
+  )
 import Effect.Class (liftEffect)
 import Effect.Exception (throw)
-import Node.ChildProcess (ChildProcess, defaultExecSyncOptions, defaultSpawnOptions, execSync, kill, onExit, spawn)
+import Node.ChildProcess
+  ( ChildProcess
+  , defaultExecSyncOptions
+  , defaultSpawnOptions
+  , execSync
+  , kill
+  , onExit
+  , spawn
+  )
 import Node.Path (FilePath)
 import Type.Prelude (Proxy(Proxy))
 
