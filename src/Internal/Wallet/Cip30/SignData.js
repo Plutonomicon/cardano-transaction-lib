e561b418a489693d7f4a87f22e66e4b02ed1fc2d
Dzmitry Shuysky <errfrom@yandex.ru>
2022-10-27 14:18:48 +0200

    KW signData: Build signature part of DataSignature

--- src/Internal/Wallet/Cip30/SignData.js
+++ src/Internal/Wallet/Cip30/SignData.js
@@ -1,23 +1,40 @@
 /* global BROWSER_RUNTIME */
 
-let lib;
+let lib, csl;
 if (typeof BROWSER_RUNTIME != "undefined" && BROWSER_RUNTIME) {
   lib = require("@emurgo/cardano-message-signing-asmjs");
+  csl = require("@emurgo/cardano-serialization-lib-browser");
 } else {
   lib = require("@emurgo/cardano-message-signing-nodejs");
+  csl = require("@emurgo/cardano-serialization-lib-nodejs");
 }
 
-// newCoseSign1Builder :: Headers -> Vec<u8> -> COSESign1Builder
-exports.newCoseSign1Builder = headers => payload => {
+// newCoseSign1Builder :: ByteArray -> Headers -> COSESign1Builder
+exports.newCoseSign1Builder = payload => headers => {
   return lib.COSESign1Builder.new(headers, payload, false);
 };
 
-// newHeaders :: ProtectedHeaderMap -> HeaderMap -> Headers
-exports.newHeaders = protectedHeaders => unprotectedHeaders => {
+// makeDataToSign :: COSESign1Builder -> ByteArray
+exports.makeDataToSign = builder => {
+  return builder.make_data_to_sign().to_bytes();
+};
+
+// sign :: PrivateKey -> ByteArray -> ByteArray
+exports.sign = privateKey => message => {
+  return privateKey.sign(message).to_bytes();
+};
+
+// buildSignature :: COSESign1Builder -> ByteArray -> ByteArray
+exports.buildSignature = builder => signedSigStruct => {
+  return builder.build(signedSigStruct).to_bytes();
+};
+
+// newHeaders :: HeaderMap -> ProtectedHeaderMap -> Headers
+exports.newHeaders = unprotectedHeaders => protectedHeaders => {
   return lib.Headers.new(protectedHeaders, unprotectedHeaders);
 };
 
-// newHeaderMap :: HeaderMap 
+// newHeaderMap :: HeaderMap
 exports.newHeaderMap = lib.HeaderMap.new();
 
 // newProtectedHeaderMap :: HeaderMap -> ProtectedHeaderMap
@@ -32,3 +49,10 @@ exports.setAlgHeaderToEdDsa = headerMap => {
   return headerMap;
 };
 
+// setAddressHeader :: ByteArray -> HeaderMap -> HeaderMap
+exports.setAddressHeader = addressBytes => headerMap => {
+  const label = lib.Label.new_text("address");
+  const value = lib.CBORValue.new_bytes(addressBytes);
+  headerMap.set_header(label, value);
+  return headerMap;
+};
