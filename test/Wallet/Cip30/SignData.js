8341065e40fe3a94061dc0d7f8569abb08d217b8
Dzmitry Shuysky <errfrom@yandex.ru>
2022-10-31 16:19:45 +0100

    Add unit tests for CIP-30 signData

--- test/Wallet/Cip30/SignData.js
+++ test/Wallet/Cip30/SignData.js
@@ -1,25 +1,52 @@
 /* global BROWSER_RUNTIME */
 
-let lib;
+let lib, csl;
 if (typeof BROWSER_RUNTIME != "undefined" && BROWSER_RUNTIME) {
   lib = require("@emurgo/cardano-message-signing-asmjs");
+  csl = require("@emurgo/cardano-serialization-lib-browser");
 } else {
   lib = require("@emurgo/cardano-message-signing-nodejs");
+  csl = require("@emurgo/cardano-serialization-lib-nodejs");
 }
 
-const fromBytes = name => helper => bytes => {
-  try {
-    return helper.just(lib[name].from_bytes(bytes));
-  } catch (_) {
-    return helper.nothing;
+function opt_chain(maybe, obj) {
+  const isNothing = x => x === null || x === undefined;
+  var result = obj;
+  for (var i = 2; i < arguments.length; i++) {
+    if (isNothing(result)) {
+      return maybe.nothing;
+    } else {
+      result = result[arguments[i]]();
+    }
   }
+  return isNothing(result) ? maybe.nothing : maybe.just(result);
+}
+
+const fromBytes = name => bytes => () => {
+  return lib[name].from_bytes(bytes);
 };
 
-// _fromBytesCoseSign1 :: MaybeFfiHelper -> CborBytes -> Maybe COSESign1
-exports._fromBytesCoseSign1 = fromBytes("COSESign1");
+// -----------------------------------------------------------------------------
+// PublicKey
+// -----------------------------------------------------------------------------
 
-// _fromBytesCoseKey :: MaybeFfiHelper -> CborBytes -> Maybe COSEKey
-exports._fromBytesCoseKey = fromBytes("COSEKey");
+// verifySignature :: COSESign1 -> PublicKey -> CborBytes -> Effect Boolean
+exports.verifySignature = coseSign1 => publicKey => sigStructBytes => () => {
+  const signature = csl.Ed25519Signature.from_bytes(coseSign1.signature());
+  return publicKey.verify(sigStructBytes, signature);
+};
+
+// -----------------------------------------------------------------------------
+// COSESign1
+// -----------------------------------------------------------------------------
+
+// _fromBytesCoseSign1 :: CborBytes -> Effect COSESign1
+exports.fromBytesCoseSign1 = fromBytes("COSESign1");
+
+// getSignedData :: COSESign1 -> Effect CborBytes
+exports.getSignedData = coseSign1 => () => {
+  return coseSign1.signed_data(null, null).to_bytes();
+};
 
 // getCoseSign1ProtectedHeaders :: COSESign1 -> HeaderMap
 const getCoseSign1ProtectedHeaders = coseSign1 => {
@@ -29,23 +56,62 @@ const getCoseSign1ProtectedHeaders = coseSign1 => {
 // getCoseSign1ProtectedHeaderAlg :: MaybeFfiHelper -> COSESign1 -> Maybe Int
 exports._getCoseSign1ProtectedHeaderAlg = maybe => coseSign1 => {
   const protectedHeaders = getCoseSign1ProtectedHeaders(coseSign1);
-  try {
-    const value = protectedHeaders.algorithm_id().as_int().as_i32();
-    return value != null ? maybe.just(value) : maybe.nothing;
-  } catch (_) {
-    return maybe.nothing;
-  }
+  return opt_chain(maybe, protectedHeaders, "algorithm_id", "as_int", "as_i32");
 };
 
 // _getCoseSign1ProtectedHeaderAddress
 //   :: MaybeFfiHelper -> COSESign1 -> Maybe CborBytes
 exports._getCoseSign1ProtectedHeaderAddress = maybe => coseSign1 => {
   const protectedHeaders = getCoseSign1ProtectedHeaders(coseSign1);
-  try {
-    const addressLabel = lib.Label.new_text("address");
-    const value = protectedHeaders.header(addressLabel).as_bytes();
-    return value != null ? maybe.just(value) : maybe.nothing;
-  } catch (_) {
-    return maybe.nothing;
-  }
+  const cborValue = protectedHeaders.header(lib.Label.new_text("address"));
+  return opt_chain(maybe, cborValue, "as_bytes");
+};
+
+// _getCoseSign1ProtectedHeaderKid
+//   :: MaybeFfiHelper -> COSESign1 -> Maybe RawBytes
+exports._getCoseSign1ProtectedHeaderKid = maybe => coseSign1 => {
+  const protectedHeaders = getCoseSign1ProtectedHeaders(coseSign1);
+  return opt_chain(maybe, protectedHeaders, "key_id");
+};
+
+// -----------------------------------------------------------------------------
+// COSEKey
+// -----------------------------------------------------------------------------
+
+// _fromBytesCoseKey :: CborBytes -> Effect COSEKey
+exports.fromBytesCoseKey = fromBytes("COSEKey");
+
+// _getCoseKeyHeaderKty :: MaybeFfiHelper -> COSEKey -> Maybe Int
+exports._getCoseKeyHeaderKty = maybe => coseKey => {
+  return opt_chain(maybe, coseKey.key_type(), "as_int", "as_i32");
+};
+
+// _getCoseKeyHeaderAlg :: MaybeFfiHelper -> COSEKey -> Maybe Int
+exports._getCoseKeyHeaderAlg = maybe => coseKey => {
+  return opt_chain(maybe, coseKey, "algorithm_id", "as_int", "as_i32");
+};
+
+// _getCoseKeyHeaderCrv :: MaybeFfiHelper -> COSEKey -> Maybe Int
+exports._getCoseKeyHeaderCrv = maybe => coseKey => {
+  const cborValue = coseKey.header(
+    lib.Label.new_int(
+      lib.Int.new_negative(lib.BigNum.from_str("1")) // crv (-1)
+    )
+  );
+  return opt_chain(maybe, cborValue, "as_int", "as_i32");
+};
+
+// _getCoseKeyHeaderX :: MaybeFfiHelper -> COSEKey -> Maybe RawBytes
+exports._getCoseKeyHeaderX = maybe => coseKey => {
+  const cborValue = coseKey.header(
+    lib.Label.new_int(
+      lib.Int.new_negative(lib.BigNum.from_str("2")) // x (-2)
+    )
+  );
+  return opt_chain(maybe, cborValue, "as_bytes");
+};
+
+// _getCoseKeyHeaderKid :: MaybeFfiHelper -> COSESign1 -> Maybe RawBytes
+exports._getCoseKeyHeaderKid = maybe => coseKey => {
+  return opt_chain(maybe, coseKey, "key_id");
 };
