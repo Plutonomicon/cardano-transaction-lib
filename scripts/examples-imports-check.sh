207a3d7777b97cb118de00fb2f2be6ec79dc282f
Dzmitry Shuysky <errfrom@yandex.ru>
2022-10-31 17:45:38 +0100

    examples-imports-check: Add ByUrl.purs to excluded_paths

--- scripts/examples-imports-check.sh
+++ scripts/examples-imports-check.sh
@@ -5,7 +5,11 @@ examples_imports_check() {
   local examples_with_internal_imports=""
   local ix=1;
 
-  for example_purs in $(find $examples_path -type f -name *.purs ! -path "$examples_path/KeyWallet/Internal/*"); do
+  set -o noglob
+  local excluded_paths=("KeyWallet/Internal/*" "ByUrl.purs")
+  local excluded_paths_=${excluded_paths[@]/#/! -path $examples_path/}
+
+  for example_purs in $(find $examples_path -type f -name *.purs ${excluded_paths_[@]}); do
     if $(grep -q "^import Ctl.Internal.*" "$example_purs"); then
       examples_with_internal_imports+="\n${ix}. ${example_purs:2}"
       ix=$((ix + 1));
