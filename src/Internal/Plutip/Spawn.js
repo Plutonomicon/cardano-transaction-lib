d07ef635f7411ba0e062faad562c257b67d099a0
peter <peter.bruehwiler@gmail.com>
2022-11-03 09:34:11 +0100

    simplify killOnExitAndRemDir

--- src/Internal/Plutip/Spawn.js
+++ src/Internal/Plutip/Spawn.js
@@ -1,11 +1,3 @@
 const fs = require("fs");
-//const path = require("path");
 
 exports._rmdirSync = path => () => fs.rmdirSync(path, { recursive: true });
-
-// exports._removeDirs = dirPath => () => {
-//   const dirs = fs.readdirSync(dirPath);
-//   for (const dir of dirs) {
-//     fs.rmdirSync(path.resolve(dirPath, dir), { recursive: true });
-//   }
-// };
