601d775ee72c966aa809edb21f3792755c1cdf9c
uhbif19 <uhbif19@gmail.com>
2022-11-03 17:28:40 +0300

    Add tmp to excluded dirs

--- whitespace_checks.sh
+++ whitespace_checks.sh
@@ -2,7 +2,7 @@
 
 # Check trailing whitespaces
 
-trailing_whitespaces=$(grep -I '[[:blank:]]$' -Rl $(find . -type f | grep -v -E '.*(spago|.git|dist-newstyle|node_modules|.node|generated-docs|dist|test-data|.spago2nix).*'))
+trailing_whitespaces=$(grep -I '[[:blank:]]$' -Rl $(find . -type f | grep -v -E '.*(tmp|spago|.git|dist-newstyle|node_modules|.node|generated-docs|dist|test-data|.spago2nix).*'))
 
 if [ -z "$trailing_whitespaces" ]
 then
@@ -15,7 +15,7 @@ fi
 
 # Check newline on end of a file
 
-no_newline=$(find . -not -path './.git/*' -not -path './.spago/*' -not -path './output/*' -not -path '*.json' -not -path '*.skey' -not -path '*node_modules*' -not -path '*.node*' -not -path '*generated-docs*' -not -path '*dist*' -not -path '*test-data*' -not -path '*spago2nix*' -type f -exec sh -c 'file -b "{}" | grep -q text' \; -exec sh -c '[ "$(tail -c 1 "{}" | od -An -a | tr -d "[:space:]")" != "nl" ]' \; -print)
+no_newline=$(find . -not -path './tmp/*' -not -path './.git/*' -not -path './.spago/*' -not -path './output/*' -not -path '*.json' -not -path '*.skey' -not -path '*node_modules*' -not -path '*.node*' -not -path '*generated-docs*' -not -path '*dist*' -not -path '*test-data*' -not -path '*spago2nix*' -type f -exec sh -c 'file -b "{}" | grep -q text' \; -exec sh -c '[ "$(tail -c 1 "{}" | od -An -a | tr -d "[:space:]")" != "nl" ]' \; -print)
 
 if [ -z "$no_newline" ]
 then
