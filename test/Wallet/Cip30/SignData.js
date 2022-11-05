5bcd99bee2b6a3dddee9e86aa8b0f14f3c558a03
Dzmitry Shuysky <errfrom@yandex.ru>
2022-11-05 19:07:05 +0100

    Switch to @emurgo/cardano-message-signing-browser

--- test/Wallet/Cip30/SignData.js
+++ test/Wallet/Cip30/SignData.js
@@ -2,7 +2,7 @@
 
 let lib, csl;
 if (typeof BROWSER_RUNTIME != "undefined" && BROWSER_RUNTIME) {
-  lib = require("@emurgo/cardano-message-signing-asmjs");
+  lib = require("@emurgo/cardano-message-signing-browser");
   csl = require("@emurgo/cardano-serialization-lib-browser");
 } else {
   lib = require("@emurgo/cardano-message-signing-nodejs");
