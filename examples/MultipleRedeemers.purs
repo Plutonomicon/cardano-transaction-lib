eaf524dbfbe6fe4b75e36a8f476418cb1852311f
uhbif19 <uhbif19@gmail.com>
2022-11-09 07:14:21 +0300

    Add Plutip tests
    * Two or more Spend redeemers
    * (At least) One each of a Spend and Mint redeemer
    * Enable test for mustSatisfyAnyOf and mustHashDatum

---
+++ examples/MultipleRedeemers.purs
@@ -0,0 +1,161 @@
+module Ctl.Examples.MultipleRedeemers
+  ( contract
+  , contractWithMintRedeemers
+  , redeemerIs1Validator
+  , redeemerIs2Validator
+  , redeemerIs3Validator
+  ) where
+
+import Contract.Prelude
+
+import Contract.Address (scriptHashAddress)
+import Contract.Monad
+  ( Contract
+  )
+import Contract.Monad (Contract, liftContractM, liftedE, liftedM, wrapContract)
+import Contract.PlutusData (PlutusData, unitDatum, unitRedeemer)
+import Contract.PlutusData
+  ( PlutusData(Integer)
+  , Redeemer(Redeemer)
+  , toData
+  , unitDatum
+  )
+import Contract.ScriptLookups as Lookups
+import Contract.Scripts
+  ( PlutusScript
+  , Validator(Validator)
+  )
+import Contract.Scripts (applyArgs, mintingPolicyHash, validatorHash)
+import Contract.TextEnvelope (decodeTextEnvelope, plutusScriptV1FromEnvelope)
+import Contract.Transaction (awaitTxConfirmed)
+import Contract.TxConstraints as Constraints
+import Contract.Utxos (getWalletBalance, utxosAt)
+import Contract.Value as Value
+import Control.Monad.Error.Class (liftMaybe)
+import Ctl.Examples.Helpers
+  ( buildBalanceSignAndSubmitTx
+  , mkCurrencySymbol
+  , mkTokenName
+  , mustPayToPubKeyStakeAddress
+  , submitAndLog
+  )
+import Ctl.Examples.MintsMultipleTokens
+  ( mintingPolicyRdmrInt2
+  )
+import Ctl.Examples.PlutusV2.ReferenceInputs (alwaysMintsPolicyV2)
+import Ctl.Examples.PlutusV2.ReferenceInputs
+  ( alwaysMintsPolicyV2
+  , mintAlwaysMintsV2ToTheScript
+  )
+import Data.BigInt as BigInt
+import Data.List as List
+import Data.Map as Map
+import Data.Traversable (sequence)
+import Effect.Exception (error)
+
+contract = do
+  tokenName <- mkTokenName "Token"
+  validator1 <- redeemerIs1Validator
+  validator2 <- redeemerIs2Validator
+  mintingPolicy <- alwaysMintsPolicyV2
+
+  -- Lock tokens on different script addresses
+
+  _ <- mintAlwaysMintsV2ToTheScript tokenName validator1 100
+  _ <- mintAlwaysMintsV2ToTheScript tokenName validator2 100
+
+  --- Unlock all those tokens with redeemer
+
+  lcs <- sequence $ spendLockedByIntOutputParams <$>
+    [ (validator1 /\ 1), (validator2 /\ 2) ]
+  let lookups = (mconcat $ snd <$> lcs) :: Lookups.ScriptLookups Void
+  let
+    constraints =
+      (mconcat $ fst <$> lcs) :: Constraints.TxConstraints Void Void
+  txHash <- buildBalanceSignAndSubmitTx
+    (Lookups.mintingPolicy mintingPolicy <> lookups)
+    constraints
+  awaitTxConfirmed txHash
+
+  --- Return
+
+  pure unit
+
+contractWithMintRedeemers = do
+  tokenName <- mkTokenName "Token"
+  validator1 <- redeemerIs1Validator
+  mintingPolicy <- alwaysMintsPolicyV2
+  mp /\ cs <- mkCurrencySymbol mintingPolicyRdmrInt2
+
+  -- Lock tokens on script address
+
+  _ <- mintAlwaysMintsV2ToTheScript tokenName validator1 100
+
+  --- Unlock tokens and mint other tokens with redeemer
+
+  lcs <- spendLockedByIntOutputParams (validator1 /\ 1)
+  let unlockingLookups = snd lcs :: Lookups.ScriptLookups Void
+  let unlockingConstraints = fst lcs :: Constraints.TxConstraints Void Void
+  let
+    mintingConstraints =
+      ( Constraints.mustMintValueWithRedeemer
+          (Redeemer $ Integer $ BigInt.fromInt 1)
+          (Value.singleton cs tokenName one)
+      )
+  txHash <- buildBalanceSignAndSubmitTx
+    ( Lookups.mintingPolicy mintingPolicy <> Lookups.mintingPolicy mp <>
+        unlockingLookups
+    )
+    (unlockingConstraints <> mintingConstraints)
+  awaitTxConfirmed txHash
+
+  --- Return
+
+  pure unit
+
+spendLockedByIntOutputParams
+  :: forall x
+   . Tuple Validator Int
+  -> Contract x
+       ( Tuple (Constraints.TxConstraints Void Void)
+           (Lookups.ScriptLookups Void)
+       )
+spendLockedByIntOutputParams (validator /\ redeemerVal) = do
+  let vhash = validatorHash validator
+  utxo <- liftContractM ("could not get utxos at " <> show vhash) =<<
+    utxosAt (scriptHashAddress vhash)
+  constraints <- pure $ mconcat do
+    input <- List.fromFoldable $ Map.keys utxo
+    pure $
+      ( Constraints.mustSpendScriptOutput input
+          $ Redeemer
+          $ toData
+          $ Integer
+          $ BigInt.fromInt redeemerVal
+      )
+  pure $ constraints /\
+    (Lookups.unspentOutputs utxo <> Lookups.validator validator)
+
+--- Importing validation scripts
+
+foreign import vredeemerInt1 :: String
+foreign import vredeemerInt2 :: String
+foreign import vredeemerInt3 :: String
+
+-- | checks whether redeemer is 1
+redeemerIs1Validator :: Contract () Validator
+redeemerIs1Validator = liftMaybe (error "Error decoding vredeemerInt1") do
+  envelope <- decodeTextEnvelope vredeemerInt1
+  Validator <$> plutusScriptV1FromEnvelope envelope
+
+-- | checks whether redeemer is 2
+redeemerIs2Validator :: Contract () Validator
+redeemerIs2Validator = liftMaybe (error "Error decoding vredeemerInt2") do
+  envelope <- decodeTextEnvelope vredeemerInt2
+  Validator <$> plutusScriptV1FromEnvelope envelope
+
+-- | checks whether redeemer is 3
+redeemerIs3Validator :: Contract () Validator
+redeemerIs3Validator = liftMaybe (error "Error decoding vredeemerInt3") do
+  envelope <- decodeTextEnvelope vredeemerInt3
+  Validator <$> plutusScriptV1FromEnvelope envelope
