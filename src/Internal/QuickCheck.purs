e41e9fffd0d32f218f76fd123bd2efa57a720cee
uhbif19 <uhbif19@gmail.com>
2022-11-13 18:45:29 +0300

    Change MustSpendTotal tests to check correct invariant and fix `Arbitrary Value`

--- src/Internal/QuickCheck.purs
+++ src/Internal/QuickCheck.purs
@@ -1,12 +1,15 @@
 -- | Utils for QuickCheck
 module Ctl.Internal.QuickCheck
-  ( unMaybeGen
+  ( genPositive
+  , unMaybeGen
   ) where
 
 import Prelude
 
 import Data.Maybe (Maybe, fromJust, isJust)
+import Data.Ord (abs)
 import Partial.Unsafe (unsafePartial)
+import Test.QuickCheck.Arbitrary (arbitrary)
 import Test.QuickCheck.Gen (Gen, resize, sized)
 
 unsafeFromJust :: forall a. Maybe a -> a
@@ -18,3 +21,6 @@ unMaybeGen gen = do
   mx <- gen
   if isJust mx then pure $ unsafeFromJust mx
   else sized (\n -> resize (n + 1) (unMaybeGen gen))
+
+genPositive :: Gen Int
+genPositive = abs <$> (arbitrary :: Gen Int)
