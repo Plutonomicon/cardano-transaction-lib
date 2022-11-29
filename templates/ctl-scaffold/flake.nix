2a8ea50035a36b36cea49e3134bd78b5a38d5f05
peter <peter.bruehwiler@gmail.com>
2022-11-29 14:30:42 +0100

    add fd to the shell

--- templates/ctl-scaffold/flake.nix
+++ templates/ctl-scaffold/flake.nix
@@ -100,6 +100,7 @@
             withRuntime = true;
             packageLockOnly = true;
             packages = with pkgs; [
+              fd
               nodePackages.eslint
               nodePackages.prettier
             ];
