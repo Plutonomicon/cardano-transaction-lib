4a252675a0b35b89f2ce5fd12a5079a7bdc953fa
Joseph Young <jy14898@my.bristol.ac.uk>
2022-12-20 10:20:42 +0000

    Apply suggestions from code review

--- nix/test-nixos-configuration.nix
+++ nix/test-nixos-configuration.nix
@@ -6,7 +6,7 @@
     memorySize = 8192;
     diskSize = 100000;
     forwardPorts = [
-      # TODO What is this port for? SSH?
+      # SSH
       { from = "host"; host.port = 2222; guest.port = 22; }
       # Ogmios
       { from = "host"; host.port = 1337; guest.port = 1337; }
