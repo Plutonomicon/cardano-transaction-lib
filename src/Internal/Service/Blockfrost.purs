9c05c08537ae33baffe0c26b4126a6b822e90fca
Dzmitry Shuysky <errfrom@yandex.ru>
2023-01-03 16:31:06 +0100

    WIP: Implement getChainTip, getEraSummaries, getSystemStart BF queries

--- src/Internal/Service/Blockfrost.purs
+++ src/Internal/Service/Blockfrost.purs
@@ -1,35 +1,61 @@
 module Ctl.Internal.Service.Blockfrost
   ( BlockfrostServiceM
   , BlockfrostServiceParams
+  , getChainTip
+  , getEraSummaries
+  , getSystemStart
   , runBlockfrostServiceM
-  , dummyExport
   ) where
 
 import Prelude
 
-import Aeson (class DecodeAeson, decodeAeson, parseJsonStringToAeson)
+import Aeson
+  ( class DecodeAeson
+  , Aeson
+  , JsonDecodeError(TypeMismatch)
+  , decodeAeson
+  , getField
+  , getFieldOptional'
+  , parseJsonStringToAeson
+  )
 import Affjax (Error, Response, URL, defaultRequest, request) as Affjax
 import Affjax.RequestBody (RequestBody) as Affjax
 import Affjax.RequestHeader (RequestHeader(ContentType, RequestHeader)) as Affjax
 import Affjax.ResponseFormat (string) as Affjax.ResponseFormat
 import Affjax.StatusCode (StatusCode(StatusCode)) as Affjax
+import Control.Monad.Except.Trans (ExceptT(ExceptT), runExceptT)
 import Control.Monad.Reader.Class (ask)
 import Control.Monad.Reader.Trans (ReaderT, runReaderT)
 import Ctl.Internal.Contract.QueryBackend (BlockfrostBackend)
+import Ctl.Internal.QueryM.Ogmios (aesonObject)
 import Ctl.Internal.ServerConfig (ServerConfig, mkHttpUrl)
 import Ctl.Internal.Service.Error
   ( ClientError(ClientHttpError, ClientHttpResponseError, ClientDecodeJsonError)
   , ServiceError(ServiceBlockfrostError)
   )
+import Ctl.Internal.Service.Helpers (aesonArray)
+import Ctl.Internal.Types.Chain (Tip(Tip, TipAtGenesis))
+import Ctl.Internal.Types.EraSummaries
+  ( EraSummaries
+  , EraSummary
+  , EraSummaryParameters
+  )
 import Data.Bifunctor (lmap)
-import Data.Either (Either(Left, Right))
+import Data.BigInt (toNumber) as BigInt
+import Data.DateTime (DateTime)
+import Data.DateTime.Instant (instant, toDateTime)
+import Data.Either (Either(Left, Right), note)
+import Data.Generic.Rep (class Generic)
 import Data.HTTP.Method (Method(GET, POST))
 import Data.Maybe (Maybe, maybe)
 import Data.MediaType (MediaType)
-import Data.Newtype (wrap)
+import Data.Newtype (class Newtype, unwrap, wrap)
+import Data.Show.Generic (genericShow)
+import Data.Time.Duration (Seconds(Seconds), convertDuration)
+import Data.Traversable (traverse)
 import Effect.Aff (Aff)
 import Effect.Aff.Class (liftAff)
-import Undefined (undefined)
+import Foreign.Object (Object)
 
 --------------------------------------------------------------------------------
 -- BlockfrostServiceM
@@ -57,15 +83,19 @@ runBlockfrostServiceM backend = flip runReaderT serviceParams
 --------------------------------------------------------------------------------
 
 data BlockfrostEndpoint
+  -- /genesis
+  = GetBlockchainGenesis
+  -- /network/eras
+  | GetEraSummaries
+  -- /blocks/latest
+  | GetLatestBlock
 
 realizeEndpoint :: BlockfrostEndpoint -> Affjax.URL
 realizeEndpoint endpoint =
   case endpoint of
-    _ -> undefined
-
-dummyExport :: Unit -> Unit
-dummyExport _ = undefined blockfrostGetRequest blockfrostPostRequest
-  (handleBlockfrostResponse undefined :: _ Int)
+    GetBlockchainGenesis -> "/genesis"
+    GetEraSummaries -> "/network/eras"
+    GetLatestBlock -> "/blocks/latest"
 
 blockfrostGetRequest
   :: BlockfrostEndpoint
@@ -121,3 +151,97 @@ handleBlockfrostResponse (Right { status: Affjax.StatusCode statusCode, body })
       body # lmap (ClientDecodeJsonError body)
         <<< (decodeAeson <=< parseJsonStringToAeson)
 
+--------------------------------------------------------------------------------
+-- Get blockchain information
+--------------------------------------------------------------------------------
+
+getSystemStart :: BlockfrostServiceM (Either ClientError DateTime)
+getSystemStart = runExceptT do
+  (systemStart :: BlockfrostSystemStart) <-
+    ExceptT $ handleBlockfrostResponse <$>
+      blockfrostGetRequest GetBlockchainGenesis
+  pure $ unwrap systemStart
+
+getChainTip :: BlockfrostServiceM (Either ClientError Tip)
+getChainTip = runExceptT do
+  (chainTip :: BlockfrostChainTip) <-
+    ExceptT $ handleBlockfrostResponse <$> blockfrostGetRequest GetLatestBlock
+  pure $ unwrap chainTip
+
+getEraSummaries :: BlockfrostServiceM (Either ClientError EraSummaries)
+getEraSummaries = runExceptT do
+  (eraSummaries :: BlockfrostEraSummaries) <-
+    ExceptT $ handleBlockfrostResponse <$> blockfrostGetRequest GetEraSummaries
+  pure $ unwrap eraSummaries
+
+--------------------------------------------------------------------------------
+-- BlockfrostSystemStart
+--------------------------------------------------------------------------------
+
+newtype BlockfrostSystemStart = BlockfrostSystemStart DateTime
+
+derive instance Generic BlockfrostSystemStart _
+derive instance Newtype BlockfrostSystemStart _
+
+instance Show BlockfrostSystemStart where
+  show = genericShow
+
+instance DecodeAeson BlockfrostSystemStart where
+  decodeAeson = aesonObject \obj -> do
+    systemStart <- Seconds <<< BigInt.toNumber <$> getField obj "system_start"
+    note (TypeMismatch "Unix timestamp")
+      (wrap <<< toDateTime <$> instant (convertDuration systemStart))
+
+--------------------------------------------------------------------------------
+-- BlockfrostChainTip
+--------------------------------------------------------------------------------
+
+newtype BlockfrostChainTip = BlockfrostChainTip Tip
+
+derive instance Generic BlockfrostChainTip _
+derive instance Newtype BlockfrostChainTip _
+
+instance Show BlockfrostChainTip where
+  show = genericShow
+
+instance DecodeAeson BlockfrostChainTip where
+  decodeAeson = aesonObject \obj -> do
+    blockHeaderHash <- wrap <$> getField obj "hash"
+    getFieldOptional' obj "slot"
+      <#> wrap
+        <<< maybe TipAtGenesis (Tip <<< wrap <<< { blockHeaderHash, slot: _ })
+
+--------------------------------------------------------------------------------
+-- BlockfrostEraSummaries
+--------------------------------------------------------------------------------
+
+newtype BlockfrostEraSummaries = BlockfrostEraSummaries EraSummaries
+
+derive instance Generic BlockfrostEraSummaries _
+derive instance Newtype BlockfrostEraSummaries _
+
+instance Show BlockfrostEraSummaries where
+  show = genericShow
+
+instance DecodeAeson BlockfrostEraSummaries where
+  decodeAeson = aesonArray (map (wrap <<< wrap) <<< traverse decodeEraSummary)
+    where
+    decodeEraSummary :: Aeson -> Either JsonDecodeError EraSummary
+    decodeEraSummary = aesonObject \obj -> do
+      start <- getField obj "start"
+      end <- getField obj "end"
+      parameters <- decodeEraSummaryParameters =<< getField obj "parameters"
+      pure $ wrap { start, end, parameters }
+
+    decodeEraSummaryParameters
+      :: Object Aeson -> Either JsonDecodeError EraSummaryParameters
+    decodeEraSummaryParameters obj = do
+      epochLength <- getField obj "epoch_length"
+      slotLength <- wrap <$> mul slotLengthFactor <$> getField obj "slot_length"
+      safeZone <- getField obj "safe_zone"
+      pure $ wrap { epochLength, slotLength, safeZone }
+      where
+      -- Blockfrost returns `slotLength` in seconds, and we use milliseconds,
+      -- so we need to convert between them.
+      slotLengthFactor :: Number
+      slotLengthFactor = 1000.0
---
+++ src/Internal/Types/EraSummaries.purs
@@ -0,0 +1,201 @@
+module Ctl.Internal.Types.EraSummaries
+  ( Epoch(Epoch)
+  , EpochLength(EpochLength)
+  , EraSummaries(EraSummaries)
+  , EraSummary(EraSummary)
+  , EraSummaryParameters(EraSummaryParameters)
+  , EraSummaryTime(EraSummaryTime)
+  , RelativeTime(RelativeTime)
+  , SafeZone(SafeZone)
+  , SlotLength(SlotLength)
+  ) where
+
+import Prelude
+
+import Aeson
+  ( class DecodeAeson
+  , class EncodeAeson
+  , encodeAeson
+  , getField
+  , partialFiniteNumber
+  )
+import Ctl.Internal.Helpers (showWithParens)
+import Ctl.Internal.Serialization.Address (Slot)
+import Ctl.Internal.Service.Helpers (aesonObject)
+import Data.BigInt (BigInt)
+import Data.Generic.Rep (class Generic)
+import Data.Maybe (Maybe)
+import Data.Newtype (class Newtype, wrap)
+import Data.Show.Generic (genericShow)
+import Partial.Unsafe (unsafePartial)
+
+--------------------------------------------------------------------------------
+-- EraSummaries
+--------------------------------------------------------------------------------
+
+newtype EraSummaries = EraSummaries (Array EraSummary)
+
+derive instance Generic EraSummaries _
+derive instance Newtype EraSummaries _
+derive instance Eq EraSummaries
+
+instance Show EraSummaries where
+  show = genericShow
+
+--------------------------------------------------------------------------------
+-- EraSummary
+--------------------------------------------------------------------------------
+
+-- | start: Era bound which captures the time, slot and epoch at which the
+-- | era starts, relative to the start of the network.
+-- |
+-- | end: Era bound which captures the time, slot and epoch at which the
+-- | era ends, relative to the start of the network.
+-- |
+-- | parameters: Era parameters that can vary across hard forks.
+newtype EraSummary = EraSummary
+  { start :: EraSummaryTime
+  , end :: Maybe EraSummaryTime
+  , parameters :: EraSummaryParameters
+  }
+
+derive instance Generic EraSummary _
+derive instance Newtype EraSummary _
+derive newtype instance Eq EraSummary
+
+instance Show EraSummary where
+  show = genericShow
+
+--------------------------------------------------------------------------------
+-- EraSummaryTime
+--------------------------------------------------------------------------------
+
+-- | time: Time in seconds relative to the start time of the network.
+-- |
+-- | slot: Absolute slot number.
+-- |
+-- | epoch: Epoch number.
+newtype EraSummaryTime = EraSummaryTime
+  { time :: RelativeTime
+  , slot :: Slot
+  , epoch :: Epoch
+  }
+
+derive instance Generic EraSummaryTime _
+derive instance Newtype EraSummaryTime _
+derive newtype instance Eq EraSummaryTime
+
+instance Show EraSummaryTime where
+  show = genericShow
+
+instance DecodeAeson EraSummaryTime where
+  decodeAeson = aesonObject \obj -> do
+    time <- getField obj "time"
+    slot <- getField obj "slot"
+    epoch <- getField obj "epoch"
+    pure $ wrap { time, slot, epoch }
+
+instance EncodeAeson EraSummaryTime where
+  encodeAeson (EraSummaryTime { time, slot, epoch }) =
+    encodeAeson { "time": time, "slot": slot, "epoch": epoch }
+
+--------------------------------------------------------------------------------
+-- EraSummaryParameters
+--------------------------------------------------------------------------------
+
+-- | epochLength: Epoch length in number of slots.
+-- |
+-- | slotLength: Slot length in milliseconds.
+-- |
+-- | safe_zone: Zone in which it is guaranteed that no hard fork can take place.
+newtype EraSummaryParameters = EraSummaryParameters
+  { epochLength :: EpochLength
+  , slotLength :: SlotLength
+  , safeZone :: SafeZone
+  }
+
+derive instance Generic EraSummaryParameters _
+derive instance Newtype EraSummaryParameters _
+derive newtype instance Eq EraSummaryParameters
+
+instance Show EraSummaryParameters where
+  show = genericShow
+
+--------------------------------------------------------------------------------
+-- RelativeTime, Epoch, EpochLength, SlotLength, SafeZone
+--------------------------------------------------------------------------------
+
+-- | A time in seconds relative to another one (typically, system start or era
+-- | start).
+newtype RelativeTime = RelativeTime Number
+
+derive instance Generic RelativeTime _
+derive instance Newtype RelativeTime _
+derive newtype instance Eq RelativeTime
+derive newtype instance Ord RelativeTime
+derive newtype instance DecodeAeson RelativeTime
+
+instance EncodeAeson RelativeTime where
+  encodeAeson (RelativeTime rt) =
+    -- We assume the numbers are finite.
+    encodeAeson $ unsafePartial partialFiniteNumber rt
+
+instance Show RelativeTime where
+  show (RelativeTime rt) = showWithParens "RelativeTime" rt
+
+-- | An epoch number or length with greater precision for Ogmios than
+-- | `Cardano.Types.Epoch`. [ 0 .. 18446744073709552000 ]
+newtype Epoch = Epoch BigInt
+
+derive instance Generic Epoch _
+derive instance Newtype Epoch _
+derive newtype instance Eq Epoch
+derive newtype instance Ord Epoch
+derive newtype instance DecodeAeson Epoch
+derive newtype instance EncodeAeson Epoch
+
+instance Show Epoch where
+  show (Epoch epoch) = showWithParens "Epoch" epoch
+
+newtype EpochLength = EpochLength BigInt
+
+derive instance Generic EpochLength _
+derive instance Newtype EpochLength _
+derive newtype instance Eq EpochLength
+derive newtype instance DecodeAeson EpochLength
+derive newtype instance EncodeAeson EpochLength
+
+instance Show EpochLength where
+  show (EpochLength epochLength) = showWithParens "EpochLength" epochLength
+
+-- | A slot length, in milliseconds.
+newtype SlotLength = SlotLength Number
+
+derive instance Generic SlotLength _
+derive instance Newtype SlotLength _
+derive newtype instance Eq SlotLength
+derive newtype instance DecodeAeson SlotLength
+
+instance EncodeAeson SlotLength where
+  encodeAeson (SlotLength sl) =
+    -- We assume the numbers are finite.
+    encodeAeson $ unsafePartial partialFiniteNumber sl
+
+instance Show SlotLength where
+  show (SlotLength slotLength) = showWithParens "SlotLength" slotLength
+
+-- | Number of slots from the tip of the ledger in which it is guaranteed that
+-- | no hard fork can take place. This should be (at least) the number of slots
+-- | in which we are guaranteed to have k blocks.
+newtype SafeZone = SafeZone BigInt
+
+derive instance Generic SafeZone _
+derive instance Newtype SafeZone _
+derive newtype instance Eq SafeZone
+derive newtype instance Semiring SafeZone
+derive newtype instance DecodeAeson SafeZone
+derive newtype instance EncodeAeson SafeZone
+
+instance Show SafeZone where
+  show (SafeZone sz) = showWithParens "SafeZone" sz
+
