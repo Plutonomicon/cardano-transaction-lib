298d9d3993ae19c113f084d8ffd632727de625dc
Dzmitry Shuysky <errfrom@yandex.ru>
2023-01-10 18:36:54 +0100

    Generate fixtures for getSystemStart

---
+++ test/Blockfrost/GenerateFixtures/SystemStart.purs
@@ -0,0 +1,39 @@
+module Test.Ctl.Blockfrost.GenerateFixtures.SystemStart (main) where
+
+import Prelude
+
+import Ctl.Internal.Service.Blockfrost
+  ( BlockfrostEndpoint(BlockchainGenesis)
+  , BlockfrostRawResponse
+  , runBlockfrostServiceTestM
+  )
+import Ctl.Internal.Service.Blockfrost (getSystemStart) as Blockfrost
+import Data.Either (either)
+import Data.Maybe (Maybe(Just, Nothing))
+import Effect (Effect)
+import Effect.Aff (Aff, launchAff_)
+import Effect.Class (liftEffect)
+import Effect.Exception (throw)
+import Test.Ctl.Blockfrost.GenerateFixtures.Helpers
+  ( blockfrostBackend
+  , storeBlockfrostFixture
+  )
+
+main :: Effect Unit
+main = launchAff_ generateFixture
+
+generateFixture :: Aff Unit
+generateFixture = do
+  backend <- liftEffect blockfrostBackend
+  sysStart <- runBlockfrostServiceTestM backend (Just onBlockfrostRawResponse)
+    Nothing
+    Blockfrost.getSystemStart
+  either (liftEffect <<< throw <<< show) (\_ -> pure unit) sysStart
+  where
+  onBlockfrostRawResponse
+    :: BlockfrostEndpoint -> BlockfrostRawResponse -> Aff Unit
+  onBlockfrostRawResponse query rawResponse =
+    case query of
+      BlockchainGenesis ->
+        storeBlockfrostFixture zero "getSystemStart" rawResponse
+      _ -> pure unit
