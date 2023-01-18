e3003b91d97ac02504f8b5e23657189b663d797b
Vladimir Kalnitsky <vladimir@mlabs.city>
2023-01-18 14:36:04 +0400

    Update the template

--- templates/ctl-scaffold/flake.nix
+++ templates/ctl-scaffold/flake.nix
@@ -10,7 +10,7 @@
       type = "github";
       owner = "Plutonomicon";
       repo = "cardano-transaction-lib";
-      rev = "27f997461fda4a6f7eb52f1165a91d7d453fb990";
+      rev = "744c3e4441e96abd0f10f57229602f251c863266";
     };
     # To use the same version of `nixpkgs` as we do
     nixpkgs.follows = "ctl/nixpkgs";
