7293fcfa2e0336f06c99f039b7d1c0a2a3256d67
Vladimir Kalnitsky <vladimir@mlabs.city>
2023-02-25 17:36:09 +0400

    Add `withoutSync` function

--- src/Contract/Sync.purs
+++ src/Contract/Sync.purs
@@ -1,9 +1,21 @@
+-- | A module for wallet/backend synchronization control.
+-- | See `doc/query-layers.md`.
 module Contract.Sync
   ( module X
+  , withoutSync
   ) where
 
+import Contract.Config (disabledSynchronizationParams)
+import Contract.Monad (Contract)
+import Control.Monad.Reader (local)
 import Ctl.Internal.BalanceTx.Sync
   ( syncBackendWithWallet
   , syncWalletWithTransaction
   , syncWalletWithTxInputs
   ) as X
+
+-- | A helper to set `synchronizationParams` to `disabledSynchronizationParams`
+-- | locally.
+withoutSync :: forall (a :: Type). Contract a -> Contract a
+withoutSync = do
+  local \cfg -> cfg { synchronizationParams = disabledSynchronizationParams }
