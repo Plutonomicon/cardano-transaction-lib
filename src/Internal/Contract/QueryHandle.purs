f1e5dd0a753277a67ddad6224b4f8fe7dc4c81cb
Vladimir Kalnitsky <vladimir@mlabs.city>
2023-03-07 14:27:57 +0400

    Move QueryHandle type to a separate module

--- src/Internal/Contract/QueryHandle.purs
+++ src/Internal/Contract/QueryHandle.purs
@@ -1,7 +1,5 @@
 module Ctl.Internal.Contract.QueryHandle
   ( getQueryHandle
-  , QueryHandle
-  , AffE
   ) where
 
 import Prelude
@@ -9,20 +7,13 @@ import Prelude
 import Contract.Log (logDebug', logWarn')
 import Control.Monad.Error.Class (throwError)
 import Control.Monad.Reader.Class (ask)
-import Ctl.Internal.Cardano.Types.ScriptRef (ScriptRef)
-import Ctl.Internal.Cardano.Types.Transaction
-  ( PoolPubKeyHash
-  , Transaction
-  , TransactionOutput
-  , UtxoMap
-  )
 import Ctl.Internal.Contract.Monad (Contract, ContractEnv, runQueryM)
 import Ctl.Internal.Contract.QueryBackend
   ( BlockfrostBackend
   , CtlBackend
   , QueryBackend(BlockfrostBackend, CtlBackend)
   )
-import Ctl.Internal.Contract.QueryHandle.Error (GetTxMetadataError)
+import Ctl.Internal.Contract.QueryHandle.Type (QueryHandle)
 import Ctl.Internal.Hashing (transactionHash) as Hashing
 import Ctl.Internal.Helpers (logWithLevel)
 import Ctl.Internal.QueryM (QueryM)
@@ -37,65 +28,26 @@ import Ctl.Internal.QueryM.Kupo
   , isTxConfirmed
   , utxosAt
   ) as Kupo
-import Ctl.Internal.QueryM.Ogmios
-  ( AdditionalUtxoSet
-  , CurrentEpoch
-  , SubmitTxR(SubmitFail, SubmitTxSuccess)
-  , TxEvaluationR
-  )
-import Ctl.Internal.QueryM.Pools (DelegationsAndRewards)
+import Ctl.Internal.QueryM.Ogmios (SubmitTxR(SubmitFail, SubmitTxSuccess))
 import Ctl.Internal.QueryM.Pools
   ( getPoolIds
   , getPubKeyHashDelegationsAndRewards
   , getValidatorHashDelegationsAndRewards
   ) as QueryM
 import Ctl.Internal.Serialization (convertTransaction, toBytes) as Serialization
-import Ctl.Internal.Serialization.Address (Address, NetworkId)
-import Ctl.Internal.Serialization.Hash (ScriptHash)
 import Ctl.Internal.Service.Blockfrost
   ( BlockfrostServiceM
   , runBlockfrostServiceM
   )
 import Ctl.Internal.Service.Blockfrost as Blockfrost
 import Ctl.Internal.Service.Error (ClientError(ClientOtherError))
-import Ctl.Internal.Types.Chain as Chain
-import Ctl.Internal.Types.Datum (DataHash, Datum)
-import Ctl.Internal.Types.EraSummaries (EraSummaries)
-import Ctl.Internal.Types.PubKeyHash (StakePubKeyHash)
-import Ctl.Internal.Types.Scripts (StakeValidatorHash)
-import Ctl.Internal.Types.Transaction (TransactionHash, TransactionInput)
-import Ctl.Internal.Types.TransactionMetadata (GeneralTransactionMetadata)
 import Data.Either (Either(Left, Right))
 import Data.Map as Map
-import Data.Maybe (Maybe, fromMaybe, isJust)
+import Data.Maybe (fromMaybe, isJust)
 import Data.Newtype (unwrap, wrap)
 import Effect.Aff (Aff)
 import Effect.Class (liftEffect)
 import Effect.Exception (error)
-
-type AffE (a :: Type) = Aff (Either ClientError a)
-
-type QueryHandle =
-  { getDatumByHash :: DataHash -> AffE (Maybe Datum)
-  , getScriptByHash :: ScriptHash -> AffE (Maybe ScriptRef)
-  , getTxMetadata ::
-      TransactionHash
-      -> Aff (Either GetTxMetadataError GeneralTransactionMetadata)
-  , getUtxoByOref :: TransactionInput -> AffE (Maybe TransactionOutput)
-  , doesTxExist :: TransactionHash -> AffE Boolean
-  , utxosAt :: Address -> AffE UtxoMap
-  , getChainTip :: AffE Chain.Tip
-  , getCurrentEpoch :: Aff CurrentEpoch
-  -- TODO Capture errors from all backends
-  , submitTx :: Transaction -> Aff (Either ClientError TransactionHash)
-  , evaluateTx :: Transaction -> AdditionalUtxoSet -> Aff TxEvaluationR
-  , getEraSummaries :: AffE EraSummaries
-  , getPoolIds :: AffE (Array PoolPubKeyHash)
-  , getPubKeyHashDelegationsAndRewards ::
-      NetworkId -> StakePubKeyHash -> AffE (Maybe DelegationsAndRewards)
-  , getValidatorHashDelegationsAndRewards ::
-      NetworkId -> StakeValidatorHash -> AffE (Maybe DelegationsAndRewards)
-  }
 
 getQueryHandle :: Contract QueryHandle
 getQueryHandle = ask <#> \contractEnv ->
