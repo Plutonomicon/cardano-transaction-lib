b85ebd4f7e288c81bdb2b7cbfc472e539f9aa4da
Daniel Farrelly <daniel@mlabs.city>
2023-05-27 01:07:41 +0100

    Revert "Export `ScriptRefByHash` from `Contract.Transaction`"
    This reverts commit 61ed2d30bef9e31dacb106485de5b6b031e0bbdc.

--- src/Contract/Transaction.purs
+++ src/Contract/Transaction.purs
@@ -87,7 +87,7 @@ import Ctl.Internal.Cardano.Types.NativeScript
       )
   ) as NativeScript
 import Ctl.Internal.Cardano.Types.ScriptRef
-  ( ScriptRef(NativeScriptRef, PlutusScriptRef, ScriptRefByHash)
+  ( ScriptRef(NativeScriptRef, PlutusScriptRef)
   , scriptRefFromMintingPolicy
   ) as ScriptRef
 import Ctl.Internal.Cardano.Types.Transaction
