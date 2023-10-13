0825b1be0c833d4504ceaa271c17ccd8f04b5e18
Vladimir Kalnitsky <vladimir@mlabs.city>
2023-10-13 19:51:09 +0400

    Remove html-webpack-plugin dependency

--- webpack.config.cjs
+++ webpack.config.cjs
@@ -2,7 +2,6 @@
 
 const path = require("path");
 const webpack = require("webpack");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
 const NodePolyfillPlugin = require("node-polyfill-webpack-plugin");
 
 module.exports = env => {
