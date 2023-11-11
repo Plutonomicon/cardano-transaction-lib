ad0341bae178818900bd336a0a2a5fe31099e98b
zmrocze <karolochmanmilarski@gmail.com>
2023-11-11 18:22:42 +0100

    New submitTxE

--- src/Contract/Backend/Ogmios.purs
+++ src/Contract/Backend/Ogmios.purs
@@ -1,6 +1,7 @@
 -- | Module for backend-specific functions that only work with Ogmios/Kupo backends
 module Contract.Backend.Ogmios
   ( getPoolParameters
+  , submitTxE
   ) where
 
 import Contract.Prelude
@@ -9,7 +10,10 @@ import Contract.Monad (Contract)
 import Contract.Transaction (PoolPubKeyHash)
 import Ctl.Internal.Cardano.Types.Transaction (PoolRegistrationParams)
 import Ctl.Internal.Contract.Monad (wrapQueryM)
-import Ctl.Internal.QueryM.Pools as QueryM
+import Ctl.Internal.QueryM (submitTxOgmios) as QueryM
+import Ctl.Internal.QueryM.Ogmios (SubmitTxR, TxHash)
+import Ctl.Internal.QueryM.Pools (getPoolParameters) as QueryM
+import Ctl.Internal.Types.CborBytes (CborBytes)
 
 -- | **This function can only run with Ogmios backend**
 -- |
@@ -19,3 +23,7 @@ getPoolParameters
   :: PoolPubKeyHash
   -> Contract PoolRegistrationParams
 getPoolParameters = wrapQueryM <<< QueryM.getPoolParameters
+
+-- | Error returning variant
+submitTxE :: TxHash -> CborBytes -> Contract SubmitTxR
+submitTxE txhash cbor = wrapQueryM $ QueryM.submitTxOgmios txhash cbor
