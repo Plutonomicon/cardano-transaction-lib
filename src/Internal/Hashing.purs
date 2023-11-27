bf06ab93be2b9fa4ebfeaa2a17e5e3906128887a
Vladimir Kalnitsky <vladimir@mlabs.city>
2023-11-27 17:23:11 +0400

    Fix redeemerHash

--- src/Internal/Hashing.purs
+++ src/Internal/Hashing.purs
@@ -4,14 +4,15 @@ module Ctl.Internal.Hashing
   , blake2b256Hash
   , blake2b256HashHex
   , datumHash
+  , hashPlutusData
   , md5HashHex
   , plutusScriptHash
+  , scriptRefHash
   , sha256Hash
   , sha256HashHex
   , sha3_256Hash
   , sha3_256HashHex
   , transactionHash
-  , scriptRefHash
   ) where
 
 import Prelude
