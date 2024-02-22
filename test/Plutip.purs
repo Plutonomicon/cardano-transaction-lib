7bd728cc3ebc6879d44508f05c0d18bc6d19cd8c
klarkc <walkerleite490@gmail.com>
2024-02-22 15:46:19 -0300

    Disable Plutip tests

--- test/Plutip.purs
+++ test/Plutip.purs
@@ -28,7 +28,7 @@ import Effect.Aff
   , effectCanceler
   , launchAff
   )
-import Mote (group, test)
+import Mote (group, test, skip)
 import Mote.Monad (mapTest)
 import Test.Ctl.BalanceTx.ChangeGeneration as ChangeGeneration
 import Test.Ctl.Plutip.Common (config)
@@ -50,7 +50,8 @@ main = interruptOnSignal SIGINT =<< launchAff do
   flip cancelWith (effectCanceler (exitCode 1)) do
     Utils.interpretWithConfig
       defaultConfig { timeout = Just $ Milliseconds 70_000.0, exit = true }
-      $ group "Plutip" do
+      -- TODO enable Test.Ctl.Plutip tests
+      $ skip $ group "Plutip" do
           testPlutipContracts config Mnemonics.suite
           group "ExUnits - normal limits" do
             testPlutipContracts config $ ExUnits.mkFailingSuite 3000
