19388c912d9e22ec6aa78cdf8d2bc775bf41dee3
Vladimir Kalnitsky <vladimir@mlabs.city>
2024-03-01 03:16:56 +0400

    Fix UtxoMinAda

--- src/Internal/BalanceTx/UtxoMinAda.purs
+++ src/Internal/BalanceTx/UtxoMinAda.purs
@@ -7,17 +7,16 @@ import Prelude
 
 import Cardano.Types.BigNum (BigNum)
 import Cardano.Types.BigNum (maxValue) as BigNum
+import Cardano.Types.Coin (Coin)
 import Cardano.Types.TransactionOutput (TransactionOutput)
 import Cardano.Types.TransactionOutput as TransactionOutput
+import Cardano.Types.Value (lovelaceValueOf)
 import Ctl.Internal.BalanceTx.FakeOutput (fakeOutputWithValue)
-import Ctl.Internal.Cardano.Types.Value (Coin(Coin), lovelaceValueOf)
 import Ctl.Internal.FfiHelpers (MaybeFfiHelper, maybeFfiHelper)
 import Ctl.Internal.Serialization.Types (DataCost)
 import Ctl.Internal.Serialization.Types (TransactionOutput) as Csl
-import Ctl.Internal.Types.ProtocolParameters
-  ( CoinsPerUtxoUnit(CoinsPerUtxoWord, CoinsPerUtxoByte)
-  )
 import Data.Maybe (Maybe, fromJust)
+import Data.Newtype (unwrap)
 import Partial.Unsafe (unsafePartial)
 
 foreign import minAdaForOutput
@@ -28,20 +27,18 @@ foreign import newCoinsPerWord :: BigNum -> DataCost
 foreign import newCoinsPerByte :: BigNum -> DataCost
 
 utxoMinAdaValue
-  :: CoinsPerUtxoUnit -> TransactionOutput -> Maybe BigNum
-utxoMinAdaValue coinsPerUtxoUnit txOutput =
+  :: Coin -> TransactionOutput -> Maybe BigNum
+utxoMinAdaValue coinsPerUtxoByte txOutput =
   let
     cslTxOutput = TransactionOutput.toCsl txOutput
-    dataCost = case coinsPerUtxoUnit of
-      CoinsPerUtxoByte (Coin n) -> newCoinsPerByte n
-      CoinsPerUtxoWord (Coin n) -> newCoinsPerWord n
+    dataCost = newCoinsPerByte (unwrap coinsPerUtxoByte)
   in
     minAdaForOutput maybeFfiHelper cslTxOutput dataCost
 
 -- useful spy: BigNum.toBigInt >>= (pure <<< spy "utxoMinAdaValue")
 
-adaOnlyUtxoMinAdaValue :: CoinsPerUtxoUnit -> BigNum
-adaOnlyUtxoMinAdaValue coinsPerUtxoUnit =
-  unsafePartial fromJust <<< utxoMinAdaValue coinsPerUtxoUnit <<<
+adaOnlyUtxoMinAdaValue :: Coin -> BigNum
+adaOnlyUtxoMinAdaValue coinsPerUtxoByte =
+  unsafePartial fromJust <<< utxoMinAdaValue coinsPerUtxoByte <<<
     fakeOutputWithValue
     $ lovelaceValueOf BigNum.maxValue
