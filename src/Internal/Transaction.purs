1983801cf2419a10bce87adde09538f9f0026ef4
Vladimir Kalnitsky <vladimir@mlabs.city>
2024-03-01 19:27:31 +0400

    Fix Transaction utils

--- src/Internal/Transaction.purs
+++ src/Internal/Transaction.purs
@@ -1,6 +1,5 @@
 module Ctl.Internal.Transaction
   ( attachDatum
-  , attachRedeemer
   , attachPlutusScript
   , attachNativeScript
   , setScriptDataHash
@@ -8,37 +7,41 @@ module Ctl.Internal.Transaction
 
 import Prelude
 
-import Cardano.Serialization.Lib (toBytes)
-import Ctl.Internal.Cardano.Types.NativeScript (NativeScript)
-import Ctl.Internal.Cardano.Types.Transaction
-  ( Costmdls
-  , Redeemer
-  , ScriptDataHash(ScriptDataHash)
-  , Transaction(Transaction)
-  , TransactionWitnessSet(TransactionWitnessSet)
-  , TxBody(TxBody)
+import Cardano.Serialization.Lib
+  ( hashScriptData
+  , packListContainer
+  , packMapContainer
   )
-import Ctl.Internal.Deserialization.WitnessSet as Deserialization.WitnessSet
-import Ctl.Internal.Serialization (hashScriptData)
-import Ctl.Internal.Serialization.PlutusData as Serialization.PlutusData
-import Ctl.Internal.Serialization.Types as Serialization
-import Ctl.Internal.Serialization.WitnessSet as Serialization.WitnessSet
-import Ctl.Internal.Types.Datum (Datum)
-import Ctl.Internal.Types.Scripts (PlutusScript)
-import Data.Array as Array
+import Cardano.Types.CostModel (CostModel)
+import Cardano.Types.CostModel as CostModel
+import Cardano.Types.Language (Language)
+import Cardano.Types.Language as Language
+import Cardano.Types.NativeScript (NativeScript)
+import Cardano.Types.PlutusData (PlutusData)
+import Cardano.Types.PlutusData as PlutusData
+import Cardano.Types.PlutusScript (PlutusScript)
+import Cardano.Types.Redeemer (Redeemer)
+import Cardano.Types.Redeemer as Redeemer
+import Cardano.Types.ScriptDataHash (ScriptDataHash(ScriptDataHash))
+import Cardano.Types.Transaction (Transaction(Transaction))
+import Cardano.Types.TransactionBody (TransactionBody(TransactionBody))
+import Cardano.Types.TransactionWitnessSet
+  ( TransactionWitnessSet(TransactionWitnessSet)
+  )
 import Data.Foldable (null)
+import Data.Map (Map)
+import Data.Map as Map
 import Data.Maybe (Maybe(Just))
 import Data.Newtype (over, unwrap)
-import Data.Traversable (traverse)
+import Data.Profunctor.Strong ((***))
 import Effect (Effect)
-import Effect.Class (liftEffect)
 
 -- | Set the `Transaction` body's script data hash. NOTE: Must include *all* of
 -- | the datums and redeemers for the given transaction
 setScriptDataHash
-  :: Costmdls
+  :: Map Language CostModel
   -> Array Redeemer
-  -> Array Datum
+  -> Array PlutusData
   -> Transaction
   -> Effect Transaction
 setScriptDataHash costModels rs ds tx@(Transaction { body, witnessSet })
@@ -52,77 +55,63 @@ setScriptDataHash costModels rs ds tx@(Transaction { body, witnessSet })
   , null rs
   , null ds = pure tx
   | otherwise = do
-      scriptDataHash <- ScriptDataHash <<< toBytes
-        <$> hashScriptData costModels rs (unwrap <$> ds)
+      let
+        costMdlsCsl =
+          packMapContainer $ map (Language.toCsl *** CostModel.toCsl) $
+            Map.toUnfoldable costModels
+        redeemersCsl =
+          packListContainer $ Redeemer.toCsl <$> rs
+        datumsCsl =
+          packListContainer $ PlutusData.toCsl <$> ds
+        scriptDataHash =
+          ScriptDataHash $ hashScriptData redeemersCsl costMdlsCsl datumsCsl
       pure $ over Transaction
         _
-          { body = over TxBody _ { scriptDataHash = Just scriptDataHash } body
+          { body = over TransactionBody
+              _ { scriptDataHash = Just scriptDataHash }
+              body
           }
         tx
 
 -- | Attach a `Datum` to a transaction by modifying its existing witness set.
 -- | Fails if either the datum or updated witness set cannot be converted during
 -- | (de-)serialization
-attachDatum :: Datum -> Transaction -> Effect Transaction
-attachDatum d = attachDatums (Array.singleton d)
+attachDatum :: PlutusData -> Transaction -> Transaction
+attachDatum d = attachDatums [ d ]
 
 attachDatums
-  :: Array Datum -> Transaction -> Effect Transaction
-attachDatums [] tx = pure tx
-attachDatums datums tx@(Transaction { witnessSet: ws }) = do
-  let ds = map (Serialization.PlutusData.convertPlutusData <<< unwrap) datums
-  updateTxWithWitnesses tx
-    <$> convertWitnessesWith ws (Serialization.WitnessSet.setPlutusData ds)
-
--- | Attach a `Redeemer` to a transaction by modifying its existing witness set.
--- | Note that this is the `Types.Transaction` representation of a redeemer and
--- | not a wrapped `PlutusData`.
---
--- | Fails if either the redeemer or updated witness set cannot be converted
--- | during (de-)serialization
-attachRedeemer
-  :: Redeemer -> Transaction -> Effect Transaction
-attachRedeemer r = attachRedeemers (Array.singleton r)
-
-attachRedeemers
-  :: Array Redeemer -> Transaction -> Effect Transaction
-attachRedeemers rs tx@(Transaction { witnessSet: ws }) = do
-  rs' <- liftEffect $ traverse Serialization.WitnessSet.convertRedeemer rs
-  updateTxWithWitnesses tx
-    <$> convertWitnessesWith ws (Serialization.WitnessSet.setRedeemers rs')
+  :: Array PlutusData -> Transaction -> Transaction
+attachDatums [] tx = tx
+attachDatums datums tx@(Transaction { witnessSet: TransactionWitnessSet ws }) =
+  do
+    updateTxWithWitnesses tx $ TransactionWitnessSet $ ws
+      { plutusData = ws.plutusData <> datums }
 
 -- | Attach a `PlutusScript` to a transaction by modifying its existing witness
 -- | set
--- |
--- | Fails if either the script or updated witness set cannot be converted
--- | during (de-)serialization
 attachPlutusScript
-  :: PlutusScript -> Transaction -> Effect Transaction
-attachPlutusScript ps = attachPlutusScripts (Array.singleton ps)
+  :: PlutusScript -> Transaction -> Transaction
+attachPlutusScript ps = attachPlutusScripts [ ps ]
 
 attachPlutusScripts
   :: Array PlutusScript
   -> Transaction
-  -> Effect Transaction
-attachPlutusScripts ps tx@(Transaction { witnessSet: ws }) = do
-  let ps' = ps # map Serialization.PlutusScript.convertPlutusScript
-  updateTxWithWitnesses tx
-    <$> convertWitnessesWith ws (Serialization.WitnessSet.setPlutusScripts ps')
+  -> Transaction
+attachPlutusScripts ps tx@(Transaction { witnessSet: TransactionWitnessSet ws }) =
+  do
+    updateTxWithWitnesses tx
+      $ TransactionWitnessSet
+      $ ws { plutusScripts = ws.plutusScripts <> ps }
 
+-- | Attach a `NativeScript` to a transaction by modifying its existing witness
+-- | set
 attachNativeScript
   :: NativeScript -> Transaction -> Transaction
-attachNativeScript ns tx = do
-  updateTxWithWitnesses tx $
-    mempty # over TransactionWitnessSet _ { nativeScripts = Just [ ns ] }
-
-convertWitnessesWith
-  :: TransactionWitnessSet
-  -> (Serialization.TransactionWitnessSet -> Effect Unit)
-  -> Effect TransactionWitnessSet
-convertWitnessesWith ws act = do
-  ws' <- liftEffect $ Serialization.WitnessSet.convertWitnessSet ws
-  liftEffect $ act ws'
-  pure $ Deserialization.WitnessSet.convertWitnessSet ws'
+attachNativeScript ns tx@(Transaction { witnessSet: TransactionWitnessSet ws }) =
+  do
+    updateTxWithWitnesses tx
+      $ TransactionWitnessSet
+      $ ws { nativeScripts = ws.nativeScripts <> [ ns ] }
 
 updateTxWithWitnesses
   :: Transaction
