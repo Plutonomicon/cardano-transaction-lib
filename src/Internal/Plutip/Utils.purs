14ca676d95dd09b543f0aaed748f3e2dc50a7bb4
Renegatto <alexey@mlabs.city>
2024-05-31 01:17:06 +0300

    Implement mkDirIfNotExists

--- src/Internal/Plutip/Utils.purs
+++ src/Internal/Plutip/Utils.purs
@@ -1,9 +1,18 @@
 module Ctl.Internal.Plutip.Utils
   ( tmpdir
+  , mkDirIfNotExists
   ) where
 
+import Prelude
 import Effect (Effect)
+import Node.FS.Sync as FS
+import Node.Path (FilePath)
 
 -- TODO: remove this function when PS bindings for os.tmpdir are available.
 -- https://github.com/Plutonomicon/cardano-transaction-lib/issues/726
 foreign import tmpdir :: Effect String
+
+mkDirIfNotExists :: FilePath -> Effect Unit
+mkDirIfNotExists dirName = do
+  exists <- FS.exists dirName
+  unless exists $ FS.mkdir dirName
