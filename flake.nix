9e55bb9e9cfabade66afd7235186f4bbe678d72d
Dzmitry Shuysky <errfrom@yandex.ru>
2024-06-20 16:20:52 +0200

    Attempt to re-enable plutip tests using older cardano-node

--- flake.nix
+++ flake.nix
@@ -34,6 +34,9 @@
 
     cardano-node.url = "github:IntersectMBO/cardano-node/8.11.0-sancho";
 
+    # TODO: Replace Plutip with cardano-testnet and remove this input
+    cardano-node-plutip.url = "github:IntersectMBO/cardano-node/8.1.1";
+
     # Repository with network parameters
     # NOTE(bladyjoker): Cardano configurations (yaml/json) often change format and break, that's why we pin to a specific known version.
     cardano-configurations = {
@@ -71,7 +74,7 @@
         iohk-nix.follows = "iohk-nix";
         haskell-nix.follows = "haskell-nix";
         hackage-nix.follows = "hackage-nix";
-        cardano-node.follows = "cardano-node";
+        cardano-node.follows = "cardano-node-plutip";
       };
     };
 
@@ -255,8 +258,9 @@
         in
         import ./plutip-server {
           inherit pkgs;
-          inherit (inputs) plutip CHaP cardano-node;
+          inherit (inputs) plutip CHaP;
           inherit (pkgs) system;
+          cardano-node = inputs.cardano-node-plutip;
           src = ./plutip-server;
         };
     in
